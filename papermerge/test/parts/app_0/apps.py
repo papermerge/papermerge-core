b3b9433b1e8e3dec7c059a30e72f820773ca620e
Eugen Ciur <eugen@papermerge.com>
2020-12-23 11:11:21 +0100

    testing part moved into main django project

--- papermerge/test/parts/app_0/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class App0Config(AppConfig):
-    name = 'app_0'
-    verbose_name = 'App Zero'
--- papermerge/test/parts/app_dr/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppDrConfig(AppConfig):
-    name = "app_dr"

f084dc9529985aa2b1dd0ff3a435d288bc6bc145
Eugen Ciur <eugen@papermerge.com>
2021-03-04 09:46:32 +0100

    add dependencies list in setup.py file

--- setup.py
+++ setup.py
@@ -3,5 +3,28 @@ from setuptools import setup, find_namespace_packages
 setup(
     packages=find_namespace_packages(
         include=['papermerge.*']
-    )
+    ),
+    install_requires=[
+        "django == 3.1",
+        "mglib == 1.3.8",
+        "mgclipboard >= 0.3.0",
+        "stapler == 1.0.0",
+        "django-dynamic-preferences == 1.10.1",
+        "celery",
+        "django-taggit",
+        "imapclient",
+        "djangorestframework",
+        "django-rest-knox",
+        "pyyaml",
+        "lxml",
+        "django-celery-results",
+        "django-allauth",
+        "django-polymorphic",
+        "django-mptt",
+        "django-polymorphic-tree",
+        "django-modelcluster",
+        "persisting-theory",
+        "django-bootstrap4",
+        "python-magic"
+    ]
 )

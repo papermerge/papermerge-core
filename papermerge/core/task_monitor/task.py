0f31d695ce3bb4dd9a29893f163ceddaf9c910e4
Eugen Ciur <eugen@papermerge.com>
2021-03-23 12:51:32 +0100

    couple more tests for task_monitor

--- papermerge/core/task_monitor/task.py
+++ papermerge/core/task_monitor/task.py
@@ -1,12 +1,50 @@
+import json
+
 
 class Task(dict):
+    """
+    A task (in context of task monitor) instance
+    is basically a named dictionary with a few extras.
+
+    Two tasks are considered equal if their names
+    matches.
+    """
 
     def __init__(self, name, **kwargs):
         self.name = name
-        self.kwargs = dict(**kwargs)
+        super().__init__(**kwargs)
 
     def __eq__(self, name):
+        """
+        Two tasks are considered equal if their names
+        matches.
+        """
         return self.name == name
+
+    def __str__(self):
+        name = self.name
+        key_values = super().__str__()
+        return f"Task(name={name}, {key_values})"
+
+    def update(self, json_str=None, **kwargs):
+        """
+        Tasks' key values can be updated either from
+        json formatted string or from
+        key/values passed by **kwargs (as for an usual python dictionary)
+        """
+        if not json_str:
+            super().update(**kwargs)
+            return self
+
+        # json formated string of length == 2
+        # is basically an empty dictionary i.e. '{}'
+        if len(json_str) <= 2:
+            return self
+
+        data = json.loads(json_str.replace("'", '"'))
+
+        super().update(data)
+        return self
 
     @property
     def short_name(self):
@@ -15,16 +53,3 @@ class Task(dict):
     @property
     def full_name(self):
         return self.name
-
-    @property
-    def state(self):
-        kwargs = self.kwargs or {}
-        return kwargs.get('state', None)
-
-    def to_dict(self):
-        ret = {}
-        ret['name'] = self.name
-        ret['state'] = self.state
-        ret.update(self.kwargs or {})
-
-        return ret
--- tests/test_task_monitor.py
+++ tests/test_task_monitor.py
@@ -7,6 +7,8 @@ class TestTasks(TestCase):
     def test_basic_task_creation(self):
         task1 = Task(name="hello")
         self.assertEquals(task1.name, "hello")
+        self.assertEquals(task1.short_name, "hello")
+        self.assertEquals(task1.full_name, "hello")
 
         task2 = Task(name="papermerge.core.tasks.ocr_page")
         self.assertEquals(
@@ -18,3 +20,63 @@ class TestTasks(TestCase):
         self.assertEquals(
             task2.full_name, "papermerge.core.tasks.ocr_page"
         )
+
+    def test_task_with_kwargs(self):
+        task1 = Task(
+            name="papermerge.core.tasks.ocr_page",
+            document_id='',
+            page_num='',
+            user_id=''
+        )
+
+        self.assertEquals(
+            task1['document_id'],
+            ''
+        )
+
+        task2 = Task(
+            name="papermerge.core.tasks.ocr_page",
+            document_id='1',
+            page_num='',
+            user_id=''
+        )
+
+        self.assertEquals(
+            task2['document_id'],
+            '1'
+        )
+        self.assertEquals(
+            task1,
+            task2
+        )
+
+    def test_task_str(self):
+        task = Task(name="xtask", document_id=1)
+
+        self.assertEquals(
+            str(task),
+            "Task(name=xtask, {'document_id': 1})"
+        )
+
+    def test_task_update(self):
+        """
+        A task can be updated in standard way i.e. as
+        standard python dictionary or via jons formated string
+        """
+        task = Task(
+            name="papermerge.core.tasks.xtask",
+            document_id=''
+        )
+        task.update(document_id=3)
+
+        self.assertEquals(
+            task['document_id'],
+            3
+        )
+
+        task.update('{"document_id": 10}')
+        self.assertEquals(
+            task['document_id'],
+            10
+        )
+

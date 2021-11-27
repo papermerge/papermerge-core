7038606cdada021cb301652d358486b6de46a1cd
Eugen Ciur <eugen@papermerge.com>
2021-11-27 07:09:06 +0100

    use python -m pytest invokation

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-PYTHONPATH=. pytest --disable-warnings
+# python -m pytest [...] adds the current directory to sys.path
+# pytest [...] DOES NOT add current path to sys.path
+poetry run python -m pytest --disable-warnings

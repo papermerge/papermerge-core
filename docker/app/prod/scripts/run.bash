13df0fc62a575def3df570f9bc3996c0dbe49b0a
Eugen Ciur <eugen@papermerge.com>
2021-12-20 07:19:41 +0100

    add init script (migrations + create superuser)

--- docker/app/prod/scripts/run.bash
+++ docker/app/prod/scripts/run.bash
@@ -3,6 +3,7 @@
 export PATH="/venv/bin:${PATH}"
 
 CMD="$1"
+MANAGE="/venv/bin/python manage.py"
 
 if [ -z $CMD ]; then
   echo "No command specified"
@@ -17,7 +18,27 @@ exec_worker() {
   exec celery --app=papermerge worker --loglevel=INFO
 }
 
+exec_init() {
+  # run migrations
+  $MANAGE migrate --no-input
+
+  # user envrironment variables:
+  #   (1) DJANGO_SUPERUSER_USERNAME
+  #   (2) DJANGO_SUPERUSER_EMAIL
+  #   (3) DJANGO_SUPERUSER_PASSWORD
+  # to create superuser if (1) and (2) are set
+  if [ -n "${DJANGO_SUPERUSER_USERNAME}" ] && [ -n "${DJANGO_SUPERUSER_EMAIL}" ]; then
+    $MANAGE createsuperuser --noinput \
+      --username ${DJANGO_SUPERUSER_USERNAME} \
+      --email ${DJANGO_SUPERUSER_EMAIL} || true
+    $MANAGE superuser -u ${DJANGO_SUPERUSER_USERNAME} --confirm || true
+  fi
+}
+
 case $CMD in
+  init)
+    exec_init
+    ;;
   server)
     exec_server
     ;;

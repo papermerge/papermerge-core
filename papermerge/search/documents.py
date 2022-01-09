91488cd719641a907353e27b601bd50e376b276a
Eugen Ciur <eugen@papermerge.com>
2022-01-09 16:33:04 +0100

    add folder and document indexes

--- papermerge/search/documents.py
+++ papermerge/search/documents.py
@@ -2,7 +2,9 @@ from django_elasticsearch_dsl import Document as ElasticSearchDocument
 from django_elasticsearch_dsl import fields as es_fields
 from django_elasticsearch_dsl.registries import registry
 
-from papermerge.core.models import Page
+from papermerge.core.models import Page as CorePage
+from papermerge.core.models import Document as CoreDocument
+from papermerge.core.models import Folder as CoreFolder
 
 
 @registry.register_document
@@ -32,9 +34,11 @@ class Page(ElasticSearchDocument):
 
     def prepare_breadcrumb(self, instance):
         doc = instance.document_version.document
-        return [
-            item.title for item in doc.get_ancestors(include_self=True)
+        list_of_titles = [
+            item.title for item in doc.get_ancestors(include_self=False)
         ]
+
+        return ' / '.join(list_of_titles)
 
     class Index:
         # Name of the Elasticsearch index
@@ -46,7 +50,7 @@ class Page(ElasticSearchDocument):
         }
 
     class Django:
-        model = Page  # The model associated with this Document
+        model = CorePage  # The model associated with this Document
 
         # The fields of the model you want to be indexed in Elasticsearch
         fields = [
@@ -54,4 +58,49 @@ class Page(ElasticSearchDocument):
             'number',
             'page_count',
             'lang'
+        ]
+
+
+@registry.register_document
+class Document(ElasticSearchDocument):
+
+    text = es_fields.TextField()
+
+    class Index:
+        # Name of the Elasticsearch index
+        name = 'documents'
+        # See Elasticsearch Indices API reference for available settings
+        settings = {
+            'number_of_shards': 1,
+            'number_of_replicas': 0
+        }
+
+    class Django:
+        model = CoreDocument  # The model associated with this Document
+
+        # The fields of the model you want to be indexed in Elasticsearch
+        fields = [
+            'title',
+            'lang',
+        ]
+
+
+@registry.register_document
+class Folder(ElasticSearchDocument):
+
+    class Index:
+        # Name of the Elasticsearch index
+        name = 'folders'
+        # See Elasticsearch Indices API reference for available settings
+        settings = {
+            'number_of_shards': 1,
+            'number_of_replicas': 0
+        }
+
+    class Django:
+        model = CoreFolder  # The model associated with this Document
+
+        # The fields of the model you want to be indexed in Elasticsearch
+        fields = [
+            'title',
         ]

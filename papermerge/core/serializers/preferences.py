04d9e532f6c72deff66682d8dda7b58335fc911b
Eugen Ciur <eugen@papermerge.com>
2022-01-21 21:03:26 +0100

    minor fix for preferences endpoint

--- papermerge/core/serializers/preferences.py
+++ papermerge/core/serializers/preferences.py
@@ -1,11 +1,14 @@
+from rest_framework import serializers
 from dynamic_preferences.users.serializers import UserPreferenceSerializer
 
 
 class CustomUserPreferenceSerializer(UserPreferenceSerializer):
+    id = serializers.CharField(read_only=True)
 
     class Meta:
         resource_name = 'preferences'
         fields = [
+            'id',
             'default',
             'value',
             'verbose_name',

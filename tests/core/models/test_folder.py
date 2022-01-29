b56846c789fa2d432c1f7ff57b032bd695ef574e
Eugen Ciur <eugen@papermerge.com>
2022-01-29 06:46:11 +0100

    add basic tests for idiefied_title

--- tests/core/models/test_folder.py
+++ tests/core/models/test_folder.py
@@ -4,6 +4,9 @@ from papermerge.core.models import User, Folder
 
 class TestFolderModel(TestCase):
 
+    def setUp(self):
+        self.user = User.objects.create_user(username="user1")
+
     def test_special_folders_are_automatically_created(self):
         """
         Assert that whenever a `core.User` is created, it gets automatically
@@ -11,7 +14,7 @@ class TestFolderModel(TestCase):
             1. home
             2. inbox
         """
-        user = User.objects.create_user(username="user1")
+        user = User.objects.create_user(username="user2")
 
         self.assertTrue(
             isinstance(user.home_folder, Folder)
@@ -24,4 +27,15 @@ class TestFolderModel(TestCase):
         )
         self.assertTrue(
             user.inbox_folder, Folder.INBOX_TITLE
+        )
+
+    def test_idified_title(self):
+        folder = Folder.objects.create(
+            title='My Documents',
+            user=self.user
+        )
+
+        self.assertEqual(
+            f'My Documents-{folder.id}',
+            folder.idified_title()
         )

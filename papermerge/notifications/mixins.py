f033bfb189234d7ce48dfa68afc18a1ad26c9ca0
Eugen Ciur <eugen@papermerge.com>
2022-06-11 09:46:33 +0200

    some comments never hurt

--- papermerge/notifications/mixins.py
+++ papermerge/notifications/mixins.py
@@ -11,4 +11,10 @@ class RequireAuth:
         async_to_sync(
             self.channel_layer.group_add
         )(self.group_name, self.channel_name)
+        # When websocket clients sends non empty 'Sec-WebSocket-Protocol'
+        # Chrome browser expects response from the server with non empty Sec-WebSocket-Protocol as well.
+        # -> self.accept() sends empty Sec-WebSocket-Protocol WebSocket() function fails with an error
+        # -> self.accept('access_token') send NON EMPTY Sec-WebSocket-Protocol header with value 'access_token'
+        # which fixes the error in Chrome Browser
+        # https://github.com/django/channels/issues/1369#issuecomment-724299511
         self.accept('access_token')

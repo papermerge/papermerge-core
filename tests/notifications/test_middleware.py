acde94aecf07acc605118771c667edb1c6dc2776
Eugen Ciur <eugen@papermerge.com>
2022-06-12 07:23:02 +0200

    PEP8

--- tests/notifications/test_middleware.py
+++ tests/notifications/test_middleware.py
@@ -13,7 +13,9 @@ class TestMiddlewareUtils(TestCase):
         assert extract_from_auth_header("Token xyz") == "xyz"
         assert extract_from_auth_header("Token abc123") == "abc123"
         assert extract_from_auth_header("Token   xy9003gz  ") == "xy9003gz"
-        assert extract_from_auth_header("   token   11xyz23sabc  ") == "11xyz23sabc"
+        assert extract_from_auth_header(
+            "   token   11xyz23sabc  "
+        ) == "11xyz23sabc"
 
     def test_extract_from_auth_header_negative(self):
         assert extract_from_auth_header(None) is None
@@ -24,16 +26,28 @@ class TestMiddlewareUtils(TestCase):
         assert extract_from_auth_header("value") is None
 
     def test_extract_sec_websocket_protocol_header_positive(self):
-        assert extract_from_sec_websocket_protocol_header("access_token, xyz") == "xyz"
-        assert extract_from_sec_websocket_protocol_header("access_token, abc123") == "abc123"
-        assert extract_from_sec_websocket_protocol_header("access_token ,  xy9003gz  ") == "xy9003gz"
-        assert extract_from_sec_websocket_protocol_header("   access_token ,  11xyz23sabc  ") == "11xyz23sabc"
+        assert extract_from_sec_websocket_protocol_header(
+            "access_token, xyz"
+        ) == "xyz"
+        assert extract_from_sec_websocket_protocol_header(
+            "access_token, abc123"
+        ) == "abc123"
+        assert extract_from_sec_websocket_protocol_header(
+            "access_token ,  xy9003gz  "
+        ) == "xy9003gz"
+        assert extract_from_sec_websocket_protocol_header(
+            "   access_token ,  11xyz23sabc  "
+        ) == "11xyz23sabc"
 
     def test_extract_sec_websocket_protocol_header_negative(self):
         assert extract_from_sec_websocket_protocol_header(None) is None
         # no comma
-        assert extract_from_sec_websocket_protocol_header("access_token   xy9003gz  ") is None
-        assert extract_from_sec_websocket_protocol_header("   access_token   11xyz23sabc  ") is None
+        assert extract_from_sec_websocket_protocol_header(
+            "access_token   xy9003gz  "
+        ) is None
+        assert extract_from_sec_websocket_protocol_header(
+            "   access_token   11xyz23sabc  "
+        ) is None
 
     def test_extract_token_positive_1(self):
         headers = [

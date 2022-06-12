4c3abd7e7c6994c14afd97d83269b8c73803eb7b
Eugen Ciur <eugen@papermerge.com>
2022-06-12 08:26:53 +0200

    fix 'apps not loaded yet' when loading daphne server

--- docker/dev/config/asgi.py
+++ docker/dev/config/asgi.py
@@ -1,9 +1,15 @@
 import os
 
+from django.core.asgi import get_asgi_application
+
+# Load django apps. This line must be before any other
+# imports, otherwise there is "Apps aren't loaded yet exception
+get_asgi_application()
+
+from papermerge.notifications.routing import websocket_urlpatterns
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 from papermerge.notifications.middleware import TokenAuthMiddleware
-from papermerge.notifications.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 

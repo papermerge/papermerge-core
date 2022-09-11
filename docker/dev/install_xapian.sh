f928486a1b068ec94e56c4bf9ea1bcecf194cf1d
Eugen Ciur <eugen@papermerge.com>
2022-09-11 08:55:34 +0200

    Xapian search works!

---
+++ docker/dev/install_xapian.sh
@@ -0,0 +1,40 @@
+VERSION=1.4.20
+
+# prepare
+mkdir $VIRTUAL_ENV/packages && cd $VIRTUAL_ENV/packages
+
+CORE=xapian-core-$VERSION
+BINDINGS=xapian-bindings-$VERSION
+
+# download
+echo "Downloading source..."
+curl -O https://oligarchy.co.uk/xapian/$VERSION/${CORE}.tar.xz
+curl -O https://oligarchy.co.uk/xapian/$VERSION/${BINDINGS}.tar.xz
+
+# extract
+echo "Extracting source..."
+tar xf ${CORE}.tar.xz
+tar xf ${BINDINGS}.tar.xz
+
+# install
+echo "Installing Xapian-core..."
+cd $VIRTUAL_ENV/packages/${CORE}
+./configure --prefix=$VIRTUAL_ENV && make && make install
+
+PYV=`python -c "import sys;t='{v[0]}'.format(v=list(sys.version_info[:1]));sys.stdout.write(t)";`
+
+if [ $PYV = "2" ]; then
+    PYTHON_FLAG=--with-python
+else
+    PYTHON_FLAG=--with-python3
+fi
+
+echo "Installing Xapian-bindings..."
+cd $VIRTUAL_ENV/packages/${BINDINGS}
+./configure --prefix=$VIRTUAL_ENV $PYTHON_FLAG XAPIAN_CONFIG=$VIRTUAL_ENV/bin/xapian-config && make && make install
+
+# clean
+# rm -rf $VIRTUAL_ENV/packages
+
+# test
+python -c "import xapian"

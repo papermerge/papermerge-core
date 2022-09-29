cda9db7f74bba4caa2c553fd3e5168e58e8b61a6
Eugen Ciur <eugen@papermerge.com>
2022-09-29 20:02:36 +0200

    fix failing tests

--- tests/search/test_search_view.py
+++ tests/search/test_search_view.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 import io
 import shutil
 import os
@@ -194,7 +195,10 @@ class SearchAfterMoveToFolder(TestCase):
     will be found and if user searches 'fish' another document will
     be revealed.
     """
-    def setUp(self):
+
+    @patch('papermerge.core.signals.ocr_document_task')
+    @patch('papermerge.core.signals.generate_page_previews_task')
+    def setUp(self, _, _x):
         super().setUp()
         self.user = User.objects.create_user(username="user")
         self.client = APIClient()

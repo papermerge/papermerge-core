30906d004a6ebe962a8fb307994f975d7b74ed0f
Eugen Ciur <eugen@papermerge.com>
2023-01-16 07:32:25 +0100

    fix failing tests

--- tests/core/backup_restore/test_backup.py
+++ tests/core/backup_restore/test_backup.py
@@ -10,15 +10,36 @@ from papermerge.test.baker_recipes import (
 
 @pytest.mark.django_db
 def test_dump_data_as_dict():
-    u1 = user_recipe.make(username='test1')
+    """
+    Basic assert that dict data dump contains all
+    user's nodes
+
+    User username=test1 has following nodes:
+        - .home/
+            - My Documents/
+                - My Invoice.pdf
+        - .inbox/
+
+    This test asserts that
+        dump_dict['users'][<index of user test1>]['nodes']
+
+    contains
+    1. titles of all user's nodes i.e. '.home', '.inbox',
+        'My Documents', 'My Invoice.pdf'
+    2. breadcrumbs of all user's nodes i.e. '.home', '.inbox',
+    '.home/My Documents', '.home/My Documents/My Invoice.pdf
+    """
+    user_test1 = user_recipe.make(username='test1')
     user_recipe.make(username='test2')
     mydocs = folder_recipe.make(
         title='My Documents',
-        parent=u1.home_folder
+        parent=user_test1.home_folder,
+        user=user_test1
     )
     document_recipe.make(
         title='My Invoice.pdf',
-        parent=mydocs
+        parent=mydocs,
+        user=user_test1
     )
 
     data_dict = dump_data_as_dict()
@@ -29,8 +50,16 @@ def test_dump_data_as_dict():
         if user['username'] == 'test1'
     ][0]
     expected_titles = [node['title'] for node in user_test_1['nodes']]
+    expected_breadcrumbs = [
+        node['breadcrumb'] for node in user_test_1['nodes']
+    ]
 
     assert '.inbox' in expected_titles
     assert '.home' in expected_titles
     assert 'My Documents' in expected_titles
     assert 'My Invoice.pdf' in expected_titles
+
+    assert '.inbox/' in expected_breadcrumbs
+    assert '.home/' in expected_breadcrumbs
+    assert '.home/My Documents/' in expected_breadcrumbs
+    assert '.home/My Documents/My Invoice.pdf' in expected_breadcrumbs

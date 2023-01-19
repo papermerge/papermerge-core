268ea07fb0d949292916a3b6560ccab550e9861d
Eugen Ciur <eugen@papermerge.com>
2023-01-19 08:47:01 +0100

    backup/restore now works!

--- papermerge/core/backup_restore/restore.py
+++ papermerge/core/backup_restore/restore.py
@@ -2,6 +2,9 @@ import logging
 import tarfile
 import json
 
+from django.conf import settings
+
+from papermerge.core.lib.path import AUX_DIR_SIDECARS, AUX_DIR_DOCS
 
 from papermerge.core.backup_restore.serializers import (
     UserSerializer,
@@ -11,9 +14,7 @@ from papermerge.core.backup_restore.serializers import (
 logger = logging.getLogger(__name__)
 
 
-def restore_data(file_path: str):
-    """Restores data from backup archive"""
-
+def restore_db_data(file_path: str) -> None:
     with tarfile.open(file_path, mode="r:gz") as file:
         backup_json = file.extractfile('backup.json')
         backup_info = json.load(backup_json)
@@ -29,3 +30,17 @@ def restore_data(file_path: str):
             tag_ser = TagSerializer(data=tag_data)
             if tag_ser.is_valid():
                 tag_ser.save()
+
+
+def restore_files(file_path: str) -> None:
+    with tarfile.open(file_path, mode="r:gz") as file:
+        for tar_info in file:
+            docs_or_sidecars_prefix = (AUX_DIR_SIDECARS, AUX_DIR_DOCS,)
+            if tar_info.name.startswith(docs_or_sidecars_prefix):
+                file.extract(tar_info, path=settings.MEDIA_ROOT)
+
+
+def restore_data(file_path: str):
+    """Restores data from backup archive"""
+    restore_db_data(file_path)
+    restore_files(file_path)

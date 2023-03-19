81ebcc46b96b66fd41a68eb43888eac3e3e530d0
Eugen Ciur <eugen@papermerge.com>
2023-03-19 07:49:04 +0100

    use jwt token (oauth2)

--- papermerge/core/routers/auth.py
+++ papermerge/core/routers/auth.py
@@ -1,11 +1,14 @@
+from django.conf import settings
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
 from typing import TypedDict
 
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from papermerge.core.models import User
 from papermerge.core.schemas.users import User as PyUser
-
+from papermerge.core.schemas.token import TokenData
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token/")
 router = APIRouter(
@@ -13,13 +16,43 @@ router = APIRouter(
     tags=["auth"],
 )
 
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 300
 
-def fake_decode_token(token):
-    return PyUser.from_orm(User.objects.first())
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+    return encoded_jwt
 
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
-    user = fake_decode_token(token)
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = PyUser.from_orm(User.objects.get(username=token_data.username))
+    if user is None:
+        raise credentials_exception
+
     return user
 
 
@@ -46,4 +79,9 @@ def login(
             detail="Incorrect username or password"
         )
 
-    return {"access_token": user.username, "token_type": "bearer"}
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}

1f62e25e89266f1fbbfcd2471272ffcc00d62381
Eugen Ciur <eugen@papermerge.com>
2023-06-06 08:15:19 +0200

    useMe hook (#156)

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,10 +4,27 @@ import './styles/globals.scss';
 
 import React from 'react';
 import Home from "./components/home";
+import { useMe } from './hooks/me';
+
 import './App.css';
 
+
 function App() {
-  return <Home />
+  const { data, error, is_loading } = useMe();
+
+  if (is_loading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error</div>
+  }
+
+  if (!data?.home_folder_id) {
+    return <div>User does not have home folder</div>;
+  }
+
+  return <Home home_folder_id={data?.home_folder_id} />
 }
 
 export default App;

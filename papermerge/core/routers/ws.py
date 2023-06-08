d736743734ffb3ca4448d6996719990bf012a39b
Eugen Ciur <eugen@papermerge.com>
2023-06-08 07:18:25 +0200

    add simple connection manager

--- papermerge/core/routers/ws.py
+++ papermerge/core/routers/ws.py
@@ -17,12 +17,31 @@ router = APIRouter(
 notif = Notification(settings.REDIS_URL, channel="cha:1")
 
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+
+manager = ConnectionManager()
+
+
 @router.websocket("/ocr")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
+    await manager.connect(websocket)
     try:
         while True:
             async for message in notif:
-                await websocket.send_text(f"Message text was: {message}")
+                await manager.send(f"Message text was: {message}", websocket)
     except WebSocketDisconnect:
         logger.info("Websocket disconnected")
+        manager.disconnect(websocket)

f942117d2b56f6f7aa98f747302144a0039c5fca
Eugen Ciur <eugen@papermerge.com>
2023-06-18 06:49:33 +0200

    fix version

--- papermerge/core/version.py
+++ papermerge/core/version.py
@@ -1,5 +1,7 @@
-import importlib.metadata
+# import importlib.metadata
 
 # In order for this to work, you need to first run:
 #   $ poetry install
-__version__ = importlib.metadata.version("papermerge-core")
+# __version__ = importlib.metadata.version("papermerge-core")
+
+__version__ = '2.1.9'

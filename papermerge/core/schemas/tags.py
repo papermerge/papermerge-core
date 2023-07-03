b0970b4beb40b22bbf5e31a3b1e62e36740817f4
Eugen Ciur <eugen@papermerge.com>
2023-07-03 07:40:59 +0200

    add couple of openapi examples

--- papermerge/core/schemas/tags.py
+++ papermerge/core/schemas/tags.py
@@ -13,6 +13,18 @@ class Tag(BaseModel):
 
     class Config:
         orm_mode = True
+        schema_extra = {
+            "example": [
+                {
+                    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "name": "important",
+                    "description": "tag for important documents",
+                    "bg_color": "#ffaaff",
+                    "fg_color": '#ff0000',
+                    'pinned': True
+                }
+            ]
+        }
 
 
 class CreateTag(BaseModel):
@@ -24,3 +36,14 @@ class CreateTag(BaseModel):
 
     class Config:
         orm_mode = True
+        schema_extra = {
+            "example": [
+                {
+                    "name": "important",
+                    "description": "tag for important documents",
+                    "bg_color": "#ffaaff",
+                    "fg_color": '#ff0000',
+                    'pinned': True
+                }
+            ]
+        }

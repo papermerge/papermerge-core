821f0375eca518f423440bfc5942c2e7d0da4692
Eugen Ciur <eugen@papermerge.com>
2023-07-10 09:01:38 +0200

    styling of document/folder list/tiles nodes (#176)

--- ui/src/components/commander/node/document.tsx
+++ ui/src/components/commander/node/document.tsx
@@ -9,7 +9,7 @@ import websockets from "../../../services/ws";
 
 import TagsComponent from './tags';
 import type { CheckboxChangeType, NodeArgsType } from "../types";
-import { OcrStatusEnum } from "types";
+import { DisplayNodesModeEnum, OcrStatusEnum } from "types";
 
 
 function str_id(node_id: string): string {
@@ -17,7 +17,14 @@ function str_id(node_id: string): string {
 }
 
 
-function Document({node, onClick, onSelect, is_loading, is_selected}: NodeArgsType) {
+function Document({
+  node,
+  onClick,
+  onSelect,
+  is_loading,
+  is_selected,
+  display_mode
+}: NodeArgsType) {
 
   const [ status, setStatus ] = useState<OcrStatusEnum>(node?.document?.ocr_status || "UNKNOWN");
   const protected_thumbnail = useProtectedJpg(node?.thumbnail_url || node?.document?.thumbnail_url || null);
@@ -54,27 +61,47 @@ function Document({node, onClick, onSelect, is_loading, is_selected}: NodeArgsTy
   } else if ( protected_thumbnail.error ) {
     thumbnail_component = <div>{protected_thumbnail.error}</div>
   } else {
-    thumbnail_component = <div>
+    thumbnail_component = <div className="image">
       {protected_thumbnail.data}
     </div>
   }
 
-  return (
-    <div className="node document" draggable>
-      {is_loading ? <Spinner />: <SpinnerPlaceholder />}
-      <div>
-        <Form.Check onChange={onselect} checked={is_selected} type="checkbox" />
-      </div>
-      <TagsComponent tags={node.tags} max_items={3}/>
-      <div className="d-flex flex-column">
-        {thumbnail_component}
-        <div className="d-flex">
+  if (display_mode == DisplayNodesModeEnum.Tiles) {
+    return (
+      <div className="node document tile" draggable>
+        <div className='checkbox'>
+            <Form.Check onChange={onselect} checked={is_selected} type="checkbox" />
+        </div>
+        <TagsComponent tags={node.tags} max_items={4}/>
+        <div className="body" onClick={onclick}>
+          {is_loading ? <Spinner />: <SpinnerPlaceholder />}
+          {thumbnail_component}
+        </div>
+        <div className="footer d-flex">
           <OcrStatus status={status} />
           <div className="title" onClick={onclick}>{node.title}</div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="node document list" draggable>
+        <div className='checkbox'>
+            <Form.Check onChange={onselect} checked={is_selected} type="checkbox" />
+        </div>
+        {is_loading ? <Spinner />: <SpinnerPlaceholder />}
+        <div className="body" onClick={onclick}>
+          {thumbnail_component}
+        </div>
+        <div className="title" onClick={onclick}>{node.title}</div>
+        <div className="ocr">
+          <OcrStatus status={status} />
+        </div>
+        <TagsComponent tags={node.tags} max_items={4}/>
+      </div>
+    );
+  }
+
 }
 
 export default Document;
--- ui/src/components/commander/node/folder.tsx
+++ ui/src/components/commander/node/folder.tsx
@@ -34,16 +34,14 @@ const Folder = forwardRef<HTMLDivElement, NodeArgsType>(
       props.onDragEnd(props.node.id, event);
     }
 
-    const css_class_display_mode = () => {
-      if (props.display_mode == DisplayNodesModeEnum.List) {
-        return 'd-flex flex-row align-items-center';
-      }
-
-      return 'd-flex flex-column';
-    }
-
     const css_class_node = (): string => {
-      let css_class = 'node folder';
+      let css_class: string = '';
+
+      if (props.display_mode == DisplayNodesModeEnum.Tiles) {
+        css_class = 'node folder tile';
+      } else {
+        css_class = 'node folder list';
+      }
 
       if (props.node.accept_dropped_nodes) {
         css_class += ' accept_dropped_nodes ';
@@ -56,30 +54,57 @@ const Folder = forwardRef<HTMLDivElement, NodeArgsType>(
       return css_class;
     }
 
-    return (
-      <>
-        <div key={props.node.id} ref={ref}
-          className={css_class_node()}
-          onDragStart={onDragStartHandle}
-          onDrag={onDragHandle}
-          onDragEnd={onDragEndHandle}
-          draggable>
-          {props.is_loading ? <Spinner />: <SpinnerPlaceholder />}
-          <div>
-            <Form.Check
-              key={props.node.id}
-              onChange={onselect}
-              checked={props.is_selected}
-              type="checkbox" />
+    if (props.display_mode == DisplayNodesModeEnum.Tiles) {
+      return (
+        <>
+          <div key={props.node.id} ref={ref}
+            className={css_class_node()}
+            onDragStart={onDragStartHandle}
+            onDrag={onDragHandle}
+            onDragEnd={onDragEndHandle}
+            draggable>
+            {props.is_loading ? <Spinner />: <SpinnerPlaceholder />}
+            <div className='checkbox'>
+              <Form.Check
+                key={props.node.id}
+                onChange={onselect}
+                checked={props.is_selected}
+                type="checkbox" />
+            </div>
+            <TagsComponent tags={props.node.tags} max_items={3}/>
+            <div className="body" onClick={onclick}>
+              <div className="icon folder"></div>
+            </div>
+            <div className="footer">
+              <div className="title" onClick={onclick}>{props.node.title}</div>
+            </div>
           </div>
-          <TagsComponent tags={props.node.tags} max_items={3}/>
-          <div className={css_class_display_mode()}>
-            <div className="icon folder"></div>
-            <div className="title" onClick={onclick}>{props.node.title}</div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div key={props.node.id} ref={ref}
+            className={css_class_node()}
+            onDragStart={onDragStartHandle}
+            onDrag={onDragHandle}
+            onDragEnd={onDragEndHandle}
+            draggable>
+              <Form.Check
+                key={props.node.id}
+                onChange={onselect}
+                checked={props.is_selected}
+                type="checkbox" />
+              {props.is_loading ? <Spinner />: <SpinnerPlaceholder />}
+              <div className="body" onClick={onclick}>
+                <div className="icon folder"></div>
+              </div>
+              <div className="title" onClick={onclick}>{props.node.title}</div>
+              <TagsComponent tags={props.node.tags} max_items={4}/>
           </div>
-        </div>
-      </>
-    );
+        </>
+      );
+    }
   }
 );
 

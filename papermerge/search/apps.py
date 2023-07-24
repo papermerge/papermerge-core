3023042a47edd30f70ef02d9d395b186788effcb
Eugen Ciur <eugen@papermerge.com>
2023-07-24 19:07:15 +0200

    minor correction

--- papermerge/search/apps.py
+++ papermerge/search/apps.py
@@ -1,10 +1,7 @@
 from django.apps import AppConfig
 
 
-class CoreConfig(AppConfig):
-    name = 'papermerge.core'
-    label = 'core'
-
-    def ready(self):
-        from papermerge.core import checks  # noqa
-        from papermerge.core import signals  # noqa
+class SearchConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'papermerge.search'
+    label = 'search'

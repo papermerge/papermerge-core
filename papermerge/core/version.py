395e542a12c23a8af0e3b118b37e7bc2e1a9c9e9
Eugen Ciur <eugen@papermerge.com>
2023-09-03 08:12:25 +0200

    Build docker images for 3.0 (#191)

--- papermerge/core/version.py
+++ papermerge/core/version.py
@@ -1,7 +1,5 @@
-# import importlib.metadata
+import importlib.metadata
 
 # In order for this to work, you need to first run:
 #   $ poetry install
-# __version__ = importlib.metadata.version("papermerge-core")
-
-__version__ = '2.1.9'
+__version__ = importlib.metadata.version("papermerge-core")

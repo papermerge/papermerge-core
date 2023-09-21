d77f5c512ae20e1c91494b1947f9a79ce812e4c6
Eugen Ciur <eugen@papermerge.com>
2023-09-21 07:43:04 +0200

    style rename and download buttons

--- ui/src/components/viewer/action_panel.tsx
+++ ui/src/components/viewer/action_panel.tsx
@@ -4,8 +4,12 @@ import { Button } from "react-bootstrap"
 export default function ActionPanel() {
   return (
     <div>
-      <Button variant="success">Rename</Button>
-      <Button variant="success">Download</Button>
+      <Button className="rounded-0 m-1" variant="success">
+        <i className="bi bi-pencil-square me-1"></i>Rename
+      </Button>
+      <Button className="rounded-0 m-1" variant="success">
+        <i className="bi bi-cloud-download me-1"></i>Download
+      </Button>
     </div>
   )
 }

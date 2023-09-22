5a9065e47d012693d9141e64f887636235c8978b
Eugen Ciur <eugen@papermerge.com>
2023-09-22 08:27:38 +0200

    wip

--- papermerge/core/page_operations.py
+++ papermerge/core/page_operations.py
@@ -1,4 +1,6 @@
+import io
 import os
+from pathlib import Path
 from typing import List
 
 from pikepdf import Pdf
@@ -6,7 +8,7 @@ from pikepdf import Pdf
 from papermerge.core.models import Page
 from papermerge.core.schemas import DocumentVersion as PyDocVer
 from papermerge.core.schemas.pages import PageAndRotOp
-from papermerge.core.storage import abs_path
+from papermerge.core.storage import abs_path, get_storage_instance
 
 
 def apply_pages_op(items: List[PageAndRotOp]):
@@ -23,16 +25,14 @@ def apply_pages_op(items: List[PageAndRotOp]):
     reorder_pdf_pages(
         old_version=old_version,
         new_version=new_version,
-        pages_data=items,
-        page_count=len(items)
+        items=items
     )
 
 
 def reorder_pdf_pages(
     old_version: PyDocVer,
     new_version: PyDocVer,
-    items: List[PageAndRotOp],
-    page_count: int
+    items: List[PageAndRotOp]
 ):
     src = Pdf.open(abs_path(old_version.document_path.url))
 
@@ -47,3 +47,47 @@ def reorder_pdf_pages(
     )
     os.makedirs(dirname, exist_ok=True)
     dst.save(abs_path(new_version.document_path.url))
+
+
+def reuse_ocr_data(uuid_map) -> None:
+    storage_instance = get_storage_instance()
+
+    for src_uuid, dst_uuid in uuid_map.items():
+        storage_instance.copy_page(
+            src=Path("pages", src_uuid),
+            dst=Path("pages", dst_uuid)
+        )
+
+
+def reuse_text_field(
+    old_version: PyDocVer,
+    new_version: PyDocVer,
+    page_map: list
+) -> None:
+    streams = collect_text_streams(
+        version=old_version,
+        # list of old_version page numbers
+        page_numbers=[item[1] for item in page_map]
+    )
+
+    # updates page.text fields and document_version.text field
+    new_version.update_text_field(streams)
+
+
+def collect_text_streams(
+    version: PyDocVer,
+    page_numbers: list[int]
+) -> list[io.StringIO]:
+    """
+    Returns list of texts of given page numbers from specified document version
+
+    Each page's text is wrapped as io.StringIO instance.
+    """
+    pages_map = {page.number: page for page in version.pages.all()}
+
+    result = [
+        io.StringIO(pages_map[number].text)
+        for number in page_numbers
+    ]
+
+    return result

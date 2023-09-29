b91d43a3072c0beafb930848d84cf5d4eb7b4bd3
Eugen Ciur <eugen@papermerge.com>
2023-09-29 07:39:10 +0200

    some comments never hurt!

--- ui/src/hooks/useToasts.ts
+++ ui/src/hooks/useToasts.ts
@@ -3,6 +3,13 @@ import { ToastContext } from "components/toasts/ToastsProvider";
 
 
 const useToast = () => {
+  /* Usage:
+
+    let toasts = useToast();
+
+    toasts?.addToast("Some notification message");
+    toasts?.removeToast(id);
+  */
   const toastHelpers = useContext(ToastContext);
   return toastHelpers;
 }

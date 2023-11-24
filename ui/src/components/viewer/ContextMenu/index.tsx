c95cf92a6da9abdf2ee77d920cf117655bc321fb
Eugen Ciur <eugen@papermerge.com>
2023-11-24 07:49:18 +0100

    hide/show ctx menu at mouse pointer

--- ui/src/components/viewer/ContextMenu/index.tsx
+++ ui/src/components/viewer/ContextMenu/index.tsx
@@ -2,14 +2,16 @@ import { useEffect, useRef, useState } from "react";
 import Dropdown from 'react-bootstrap/Dropdown';
 
 type Coord = {
-  clientX: number;
-  clientY: number;
+  x: number;
+  y: number;
 }
 
 
 export default function ContextMenu() {
-  const ref = useRef<HTMLDivElement>(null);
+  const [position, setPosition] = useState<Coord>({x: 0, y: 0})
   const [isVisible, setVisible] = useState(false);
+  const ref = useRef<HTMLDivElement>(null)
+
 
   const clickOutsideCallback = () => {
     setVisible(false);
@@ -19,29 +21,33 @@ export default function ContextMenu() {
     const new_visibility = !isVisible;
     ev.preventDefault(); // prevents default context menu
 
-    setVisible(new_visibility);
+    let new_y = ev.clientY;
+    let new_x = ev.clientX;
 
     if (new_visibility) {
-      if (ref && ref.current) {
-        ref.current.style.top = `${ev.clientY + 2}px`;
-        ref.current.style.left = `${ev.clientX + 2}px`;
-      }
+      setPosition({y: new_y, x: new_x})
     }
+
+    setVisible(new_visibility);
   }
 
   useEffect(() => {
     document.addEventListener('mousedown', clickOutsideCallback);
     // detect right click outside
     document.addEventListener('contextmenu', onRightClick);
+
     return () => {
       document.removeEventListener('mousedown', clickOutsideCallback);
       document.removeEventListener('contextmenu', onRightClick);
     }
   }, []);
 
-  return <Dropdown.Menu ref={ref} show={isVisible}>
+  return <Dropdown.Menu
+      ref={ref}
+      show={isVisible}
+      style={{top: `${position.y}px`, left: `${position.x}px`}}>
     <Dropdown.Header>Dropdown header</Dropdown.Header>
-    <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-    <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+    <Dropdown.Item eventKey="2">Move To Other Panel</Dropdown.Item>
+    <Dropdown.Item eventKey="3">Delete Document</Dropdown.Item>
   </Dropdown.Menu>
 }

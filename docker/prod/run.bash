c493852df0280c9cc625565aafcfc06f61ea5fc3
Eugen Ciur <eugen@papermerge.com>
2024-02-26 07:25:39 +0100

    change docker file to support remote user

--- docker/prod/run.bash
+++ docker/prod/run.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 CMD="$1"
-MANAGE="cd /core_ui && poetry run manage.py"
 
 mkdir -p /db  # default database is /db/db.sqlite3
 
@@ -9,10 +8,6 @@ if [ -z $CMD ]; then
   echo "No command specified"
   exit 1
 fi
-
-exec_server() {
-  exec /usr/bin/supervisord
-}
 
 exec_migrate() {
   # run migrations
@@ -35,8 +30,22 @@ exec_index_schema_apply() {
 exec_init() {
   VIRTUAL_ENV=/core_app/.venv && cd /core_app && poetry install
   exec_migrate
-  exec_createsuperuser
+  if [[ -z "${PAPERMERGE__AUTH__REMOTE}" ]]; then
+    # authenticate using internal auth server
+    exec_createsuperuser
+  fi
 }
+
+rm -f /etc/nginx/nginx.conf
+rm -f /etc/papermerge/supervisord.conf
+
+if [[ -z "${PAPERMERGE__AUTH__REMOTE}" ]]; then
+  ln -s /etc/papermerge/supervisord.default.conf /etc/papermerge/supervisord.conf
+  ln -s /etc/nginx/nginx.default.conf /etc/nginx/nginx.conf
+else
+  ln -s /etc/papermerge/supervisord.remote.conf /etc/papermerge/supervisord.conf
+  ln -s /etc/nginx/nginx.remote.conf /etc/nginx/nginx.conf
+fi
 
 case $CMD in
   init)
@@ -52,7 +61,7 @@ case $CMD in
     exec_init
     VIRTUAL_ENV=/core_app/.venv && cd /core_app && poetry run ./manage.py index_schema apply
     roco > /usr/share/nginx/html/auth_server/papermerge-runtime-config.js
-    exec /usr/bin/supervisord
+    exec /usr/bin/supervisord -c /etc/papermerge/supervisord.conf
     ;;
   worker)
     exec_init

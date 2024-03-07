3c1d81b34e8a7404c4b9c2c4def31eb842dc28ef
Eugen Ciur <eugen@papermerge.com>
2024-03-07 07:34:51 +0100

    minor fixes

--- papermerge/core/cli/token.py
+++ papermerge/core/cli/token.py
@@ -3,7 +3,6 @@ from rich import print
 from rich.pretty import pprint
 
 from papermerge.core import db, schemas, utils
-from papermerge.core.db.users import get_user
 
 app = typer.Typer(help="Basic JWT tokens management")
 engine = db.get_engine()
@@ -11,8 +10,13 @@ engine = db.get_engine()
 
 @app.command()
 def encode(username: str, scopes: str = None):
-    """Encodes token payload for given username"""
-    user: schemas.User = get_user(engine, username)
+    """Encodes JWT token payload for given username"""
+    try:
+        user: schemas.User = db.get_user(engine, username)
+    except db.UserNotFound:
+        print("[red]User not found[/red]")
+        return
+
     if scopes is None:
         scopes = []
     else:
@@ -30,12 +34,22 @@ def encode(username: str, scopes: str = None):
 
 
 @app.command()
-def decode(token: str):
-    """Decode payload of given token"""
-    if '.' in token:
-        print("[red]Doesn't look like jwt token payload[/red]")
+def decode(token_payload: str):
+    """Decode JWT token payload
+
+    Note that JWT token has three parts, delimited by dot character.
+    For this command you need to provider only the middle part (i.e. payload).
+    """
+    if '.' in token_payload:
+        print("[red]Doesn't look like JWT token payload[/red]")
         print("JWT payload do not contain dots")
         return
 
-    decoded_token_payload = utils.base64.decode(token)
+    try:
+        decoded_token_payload = utils.base64.decode(token_payload)
+    except Exception as ex:
+        print(f"[red]Decoding failed: {ex}[/red] ")
+        print("Maybe input is not base64 string?")
+        return
+
     pprint(decoded_token_payload, expand_all=True)
--- papermerge/core/db/__init__.py
+++ papermerge/core/db/__init__.py
@@ -3,14 +3,16 @@ from sqlalchemy import Engine
 from .doc import get_doc
 from .doc_ver import get_doc_ver, get_last_doc_ver
 from .engine import get_engine
+from .exceptions import UserNotFound
 from .folders import get_folder
 from .nodes import get_paginated_nodes
 from .pages import get_first_page, get_page
-from .users import create_user, get_user
+from .users import create_user, get_user, get_users
 
 __all__ = [
     'get_engine',
     'get_user',
+    'get_users',
     'create_user',
     'get_folder',
     'get_first_page',
@@ -19,5 +21,6 @@ __all__ = [
     'get_doc_ver',
     'get_doc',
     'get_paginated_nodes',
-    'Engine'
+    'Engine',
+    'UserNotFound'
 ]

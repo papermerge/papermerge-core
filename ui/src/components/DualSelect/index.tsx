80931d213c0d327d99ab104c6459ca794d69bb4f
Eugen Ciur <eugen@papermerge.com>
2024-03-10 07:52:30 +0100

    support dual select panel

--- ui/src/components/DualSelect/index.tsx
+++ ui/src/components/DualSelect/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -8,14 +9,72 @@ import { ScopeType } from 'types';
 import { Button } from 'react-bootstrap';
 import InputGroup from 'react-bootstrap/InputGroup';
 import "./dual-select.scss";
+import { useEffect, useState } from 'react';
 
 
 function DualSelect() {
-  const scopes = useResource<ScopeType>("/api/scopes/")
+  const vow = useResource<ScopeType>("/api/scopes/")
+  const [leftPanelItems, setLeftPanelItems] = useState<Array<SelectItem>>([]);
+  const [leftPanelSelectedItems, setLeftPanelSelectedItems] = useState<Array<SelectItem>>([]);
+  const [rightPanelItems, setRightPanelItems] = useState<Array<SelectItem>>([]);
+  const [rightPanelSelectedItems, setRightPanelSelectedItems] = useState<Array<SelectItem>>([]);
 
-  if (scopes.is_pending) {
+  useEffect(() => {
+    if (vow.data == null) {
+      return;
+    }
+    let selectItems: Array<SelectItem> = [];
+
+    for (const i of Object.entries(vow.data)) {
+      selectItems.push({
+        key: i[0],
+        value: i[1]
+      });
+    }
+
+    selectItems.sort((a: SelectItem, b: SelectItem) => {
+      if (a.key < b.key) {
+        return -1;
+      } else if (a.key > b.key) {
+        return 1;
+      }
+      return 0;
+    });
+
+    setLeftPanelItems(selectItems);
+  }, [vow.data]);
+
+  const onChangeRight = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const select = e.target;
+    let selectedItems: Array<SelectItem> = [];
+
+    for (const option of select.options) {
+      if (option.selected) {
+        selectedItems.push({
+          key: option.value,
+          value: option.label
+        })
+      }
+    }
+    setRightPanelSelectedItems(selectedItems);
+  }
+
+  const onMoveToRight = () => {
+  }
+
+  const onMoveToLeft = () => {
+  }
+
+  const onMoveAllToRight = () => {
+  }
+
+  const onMoveAllToLeft = () => {
+  }
+
+  if (vow.is_pending) {
     return <div>Loading...</div> // or some sort of placeholder here
   }
+
 
   return(
     <Container>
@@ -30,19 +89,16 @@ function DualSelect() {
       </Row>
       <Row>
         <Col>
-          <Form.Select className='dual-select mt-2' multiple aria-label="Default select example">
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-            <option value="4">Four</option>
-            <option value="5">Five</option>
-            <option value="6">Six</option>
-            <option value="7">Seven</option>
-            <option value="8">Eight</option>
-          </Form.Select>
+          <Select
+            items={leftPanelItems}
+            onChange={onChangeRight}/>
         </Col>
         <Col xs={1}>
-           <MoveButtons />
+           <MoveButtons
+            onMoveToRight={onMoveToRight}
+            onMoveToLeft={onMoveToLeft}
+            onMoveAllToRight={onMoveAllToRight}
+            onMoveAllToLeft={onMoveAllToLeft} />
         </Col>
         <Col>
           <Form.Select className='dual-select mt-2' multiple aria-label="Default select example">
@@ -64,22 +120,57 @@ function Filter() {
   </InputGroup>
 }
 
+type MoveButtonsArgs = {
+  onMoveToRight: () => void;
+  onMoveToLeft: () => void;
+  onMoveAllToRight: () => void;
+  onMoveAllToLeft: () => void;
+}
 
-function MoveButtons() {
+
+function MoveButtons({
+  onMoveToRight,
+  onMoveToLeft,
+  onMoveAllToRight,
+  onMoveAllToLeft
+}: MoveButtonsArgs) {
   return <Stack gap={2}>
-    <Button size='sm' variant='light'>
+    <Button size='sm' variant='light' onClick={onMoveToLeft}>
       <span className='bi bi-chevron-left'></span>
     </Button>
-    <Button size='sm' variant='light'>
+    <Button size='sm' variant='light' onClick={onMoveToRight}>
       <span className='bi bi-chevron-right'></span>
     </Button>
-    <Button size='sm' variant='light'>
+    <Button size='sm' variant='light' onClick={onMoveAllToLeft}>
       <span className='bi bi-chevron-double-left'></span>
     </Button>
-    <Button size='sm' variant='light'>
+    <Button size='sm' variant='light' onClick={onMoveAllToRight}>
       <span className='bi bi-chevron-double-right'></span>
     </Button>
   </Stack>
+}
+
+type SelectItem = {
+  key: string;
+  value: string;
+}
+
+type SelectArgs = {
+  items: Array<SelectItem>;
+  onChange: React.ChangeEventHandler<HTMLSelectElement>;
+}
+
+
+function Select({items, onChange}: SelectArgs) {
+  const listItems = items.map(
+    item => <option key={item.key} value={item.key}>{item.value}</option>
+  );
+
+  return <Form.Select
+      className='dual-select mt-2' multiple
+      onChange={onChange}>
+    {listItems}
+  </Form.Select>
 }
 
 export default DualSelect;

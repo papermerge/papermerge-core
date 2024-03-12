10e52133ba61415b323f79d51b2bc8084a671a5f
Eugen Ciur <eugen@papermerge.com>
2024-03-12 08:31:51 +0100

    add test for permissions cascade deletion issue

--- tests/core/models/test_group.py
+++ tests/core/models/test_group.py
@@ -5,7 +5,9 @@ from papermerge.core import db
 
 
 def test_group_create(db_engine: Engine):
-    sync_scopes(db_engine)
+    Session = sessionmaker(db_engine)
+    with Session() as session:
+        db.sync_perms(session)
 
     scopes = {"tag.update", "tag.create", "tag.delete"}
 
@@ -19,8 +21,27 @@ def test_group_create(db_engine: Engine):
     assert group_details.name == 'G1'
     assert set(group_details.scopes) == scopes
 
+    db.delete_group(db_engine, group.id)
 
-def sync_scopes(db_engine):
+
+def test_group_create_and_delete(db_engine: Engine):
+    """Deleting a group should preserve existing permission models"""
     Session = sessionmaker(db_engine)
     with Session() as session:
         db.sync_perms(session)
+        initial_perms_count = len(db.get_perms(session))
+
+    scopes = {"tag.update", "tag.create", "tag.delete"}
+
+    group = db.create_group(
+        db_engine,
+        "G1",
+        scopes=list(scopes)
+    )
+    db.delete_group(db_engine, group_id=group.id)
+
+    with Session() as session:
+        perms_count = len(db.get_perms(session))
+        # the `db.delete_group` should not affect
+        # permissions count
+        assert perms_count == initial_perms_count

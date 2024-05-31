e1fb35dee637fa8bf8de2767750704147db9f9fc
Elman Torres <63641794+wela-elman@users.noreply.github.com>
2024-05-31 10:27:30 +0800

    Update sidebar.tsx

--- ui/src/components/sidebar/sidebar.tsx
+++ ui/src/components/sidebar/sidebar.tsx
@@ -36,8 +36,8 @@ function SidebarOpened({onClick, current, scopes}: SidebarArgs) {
   return (
     <div className="sidebar opened d-flex flex-column flex-shrink-0 text-white bg-dark">
       <a className='navbar-brand m-2' href="#">
-        <img src="/images/papermerge1.svg" width="80" />
-        <p className='mt-2 text-center'>Papermerge DMS</p>
+        <img src="/images/wela-logo-landscape-2-conv.svg" width="80" />
+        <p className='mt-2 text-center'>Papermerge</p>
       </a>
       <hr />
       <Nav>

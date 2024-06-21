2453ebe4db3d4feff7104b27ed6bf2943e18c7d0
Eugen Ciur <eugen@papermerge.com>
2024-06-21 09:01:31 +0200

    use relative paths

--- ui2/src/slices/usersSlice.ts
+++ ui2/src/slices/usersSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios';
-import type { User } from "../types"
+import type { User } from "@/types"
 
 const initialState: any = []
 

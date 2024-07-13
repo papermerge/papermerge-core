8dab0ae4f310cb876203c8ade3acaa7a90958e84
Eugen Ciur <eugen@papermerge.com>
2024-07-13 08:36:27 +0200

    UI2: upload documents

--- ui2/src/components/Commander/UploadButton.tsx
+++ ui2/src/components/Commander/UploadButton.tsx
@@ -2,13 +2,22 @@ import {FileButton, ActionIcon, Tooltip} from "@mantine/core"
 import {IconUpload} from "@tabler/icons-react"
 
 export default function UploadButton() {
-  const onUpload = () => {}
+  const onUpload = (files: File[]) => {
+    if (!files) {
+      console.error("Empty array for uploaded files")
+      return
+    }
+  }
 
   return (
-    <FileButton onChange={onUpload} multiple>
-      {() => (
+    <FileButton
+      onChange={onUpload}
+      accept="image/png,image/jpeg,application/pdf"
+      multiple
+    >
+      {props => (
         <Tooltip label="Upload" withArrow>
-          <ActionIcon size="lg" variant="default">
+          <ActionIcon {...props} size="lg" variant="default">
             <IconUpload stroke={1.4} />
           </ActionIcon>
         </Tooltip>

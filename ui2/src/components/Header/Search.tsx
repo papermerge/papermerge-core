cf9737486d86c9931d8e0338bf4a759d2b2d0c6d
Eugen Ciur <eugen@papermerge.com>
2024-08-01 09:28:26 +0200

    basic search - wip

--- ui2/src/components/Header/Search.tsx
+++ ui2/src/components/Header/Search.tsx
@@ -1,23 +1,40 @@
-import {Autocomplete, rem} from "@mantine/core"
+import {useState} from "react"
+import {Input, CloseButton, rem} from "@mantine/core"
 import {IconSearch} from "@tabler/icons-react"
 
 export default function Search() {
+  const [value, setValue] = useState("Clear me")
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.value)
+  }
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const {key} = event
+
+    if (key === "Enter") {
+      console.log(`Enter pressed. Query = ${value}`)
+    }
+  }
+
   return (
-    <Autocomplete
+    <Input
       placeholder="Search"
+      value={value}
+      onChange={onChange}
+      onKeyDown={onKeyDown}
       leftSection={
         <IconSearch style={{width: rem(16), height: rem(16)}} stroke={1.5} />
       }
-      data={[
-        "billing.pdf",
-        "anmeldung.pdf",
-        "tags:important",
-        "ciur AND tags:important",
-        "My Documents",
-        "Clients",
-        "brother_007556.pdf"
-      ]}
-      visibleFrom="xs"
+      rightSectionPointerEvents="all"
+      mt="md"
+      rightSection={
+        <CloseButton
+          aria-label="Clear input"
+          onClick={() => setValue("")}
+          style={{display: value ? undefined : "none"}}
+        />
+      }
     />
   )
 }

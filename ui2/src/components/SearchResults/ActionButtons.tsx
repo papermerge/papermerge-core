800131654099ab06c1b003f89ff8b990c709e75c
Eugen Ciur <eugen@papermerge.com>
2024-08-02 08:39:10 +0200

    it works!

--- ui2/src/components/SearchResults/ActionButtons.tsx
+++ ui2/src/components/SearchResults/ActionButtons.tsx
@@ -1,10 +1,13 @@
 import {Group} from "@mantine/core"
 import ToggleSecondaryPanel from "@/components/DualPanel/ToggleSecondaryPanel"
+import GoBackButton from "./GoBackButton"
 
 export default function ActionButtons() {
   return (
     <Group justify="space-between">
-      <Group>Some button here</Group>
+      <Group>
+        <GoBackButton />
+      </Group>
       <Group>
         <ToggleSecondaryPanel />
       </Group>

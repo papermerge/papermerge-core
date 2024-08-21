2407c2d1e028d0bcb159f347d25329a32ead9e69
Eugen Ciur <eugen@papermerge.com>
2024-08-21 08:13:38 +0200

    minor fixes

--- ui2/src/httpClient.ts
+++ ui2/src/httpClient.ts
@@ -14,6 +14,17 @@ client.defaults.headers.common = defaultHeaders
 
 export default client
 
+function get_file_ext(file_name: string): string {
+  var arr = file_name.split(".")
+  return arr.pop()!
+}
+
+function get_file_basename(file_name: string): string {
+  var arr = file_name.split(".")
+  arr.pop()
+  return arr.join(".")
+}
+
 /*
 download_file of specific document version
 
@@ -21,7 +32,6 @@ First gets the current `download_url`, as in case of S3 storage
 (S3 private storage actually) there expiration keys are
 valid for couple of minutes only
 */
-
 async function download_file(doc_ver: DocumentVersion) {
   const resp1 = await client.get(`/api/document-versions/${doc_ver.id}`)
   const v = resp1.data as DocumentVersion
@@ -36,8 +46,11 @@ async function download_file(doc_ver: DocumentVersion) {
     - https://stackoverflow.com/a/78401145
   */
   let a = document.createElement("a")
+  const ext = get_file_ext(v.file_name)
+  const basename = get_file_basename(v.file_name)
+
   a.href = url
-  a.download = doc_ver.file_name
+  a.download = `${basename}-v${v.number}-${v.short_description}.${ext}`
   // we need to append the element to the dom -> otherwise it will not work in firefox
   document.body.appendChild(a)
   a.click()

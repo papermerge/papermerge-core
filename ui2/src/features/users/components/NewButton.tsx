d6e2ea51bd58be32f8fef7e93cd8e88d501613df
Eugen Ciur <eugen@papermerge.com>
2024-09-03 06:35:47 +0200

    Obsolete generic modals for user feature (#445)

--- ui2/src/features/users/components/NewButton.tsx
+++ ui2/src/features/users/components/NewButton.tsx
@@ -1,15 +1,17 @@
+import {useDisclosure} from "@mantine/hooks"
 import {Button} from "@mantine/core"
 import {IconPlus} from "@tabler/icons-react"
-import {openModal} from "@/components/modals/Generic"
 import NewUserModal from "./NewUserModal"
 
 export default function NewButton() {
-  const onClick = () => {
-    openModal<any, {userId: string}>(NewUserModal)
-  }
+  const [opened, {open, close}] = useDisclosure(false)
+
   return (
-    <Button leftSection={<IconPlus />} onClick={onClick} variant="default">
-      New
-    </Button>
+    <>
+      <Button leftSection={<IconPlus />} onClick={open} variant="default">
+        New
+      </Button>
+      <NewUserModal opened={opened} onSubmit={close} onCancel={close} />
+    </>
   )
 }

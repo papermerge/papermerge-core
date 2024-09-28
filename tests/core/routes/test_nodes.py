e45775d8409d78a9588e322fd073b38ced35a7db
Eugen Ciur <eugen@papermerge.com>
2024-09-28 09:38:11 +0200

    experimenting with ruff/black

--- tests/core/routes/test_nodes.py
+++ tests/core/routes/test_nodes.py
@@ -9,14 +9,18 @@ from papermerge.test.types import AuthTestClient
 
 
 @pytest.mark.django_db(transaction=True)
-def test_initial_users_home_folder_is_empty(montaigne: User, api_client: TestClient):
+def test_initial_users_home_folder_is_empty(
+    montaigne: User, api_client: TestClient
+):
     parent_id = montaigne.home_folder.id
     response = api_client.get(
         f"/nodes/{parent_id}", headers={"Remote-User": "montaigne"}
     )
     assert response.status_code == 200
 
-    data = PaginatedResponse[schemas.Document | schemas.Folder](**response.json())
+    data = PaginatedResponse[schemas.Document | schemas.Folder](
+        **response.json()
+    )
     assert data.items == []
 
 
@@ -37,14 +41,18 @@ def test_basic_sorting_by_title(auth_api_client: AuthTestClient):
 
     # Check "ASC" part; first returned item must be A, and second B
     response = auth_api_client.get(f"/nodes/{home.id}?order_by=title")
-    data = PaginatedResponse[schemas.Document | schemas.Folder](**response.json())
+    data = PaginatedResponse[schemas.Document | schemas.Folder](
+        **response.json()
+    )
     assert len(data.items) == 2
     assert data.items[0].title == "A"
     assert data.items[1].title == "B"
 
     # Check "DESC" part; first returned item must be B, and second A
     response = auth_api_client.get(f"/nodes/{home.id}?order_by=-title")
-    data = PaginatedResponse[schemas.Document | schemas.Folder](**response.json())
+    data = PaginatedResponse[schemas.Document | schemas.Folder](
+        **response.json()
+    )
     assert len(data.items) == 2
     assert data.items[0].title == "B"
     assert data.items[1].title == "A"
@@ -63,18 +71,24 @@ def test_basic_sorting_by_ctype(auth_api_client: AuthTestClient):
     """
     home = auth_api_client.user.home_folder
     folder_recipe.make(title="A", user=auth_api_client.user, parent=home)
-    document_recipe.make(title="invoice.pdf", user=auth_api_client.user, parent=home)
+    document_recipe.make(
+        title="invoice.pdf", user=auth_api_client.user, parent=home
+    )
 
     # Check "ASC" part; first returned document item, and second the folder
     response = auth_api_client.get(f"/nodes/{home.id}?order_by=ctype")
-    data = PaginatedResponse[schemas.Document | schemas.Folder](**response.json())
+    data = PaginatedResponse[schemas.Document | schemas.Folder](
+        **response.json()
+    )
     assert len(data.items) == 2
     assert data.items[0].ctype == "document"
     assert data.items[1].ctype == "folder"
 
     # Check "DESC" part; first returned folder item, and second the document
     response = auth_api_client.get(f"/nodes/{home.id}?order_by=-ctype")
-    data = PaginatedResponse[schemas.Document | schemas.Folder](**response.json())
+    data = PaginatedResponse[schemas.Document | schemas.Folder](
+        **response.json()
+    )
     assert len(data.items) == 2
     assert data.items[0].ctype == "folder"
     assert data.items[1].ctype == "document"

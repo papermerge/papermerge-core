9fb4682fd176c823faac3d5019c2bf6910c33729
Eugen Ciur <eugen@papermerge.com>
2024-10-03 10:51:05 +0200

    draft for document custom field update

--- papermerge/core/db/doc.py
+++ papermerge/core/db/doc.py
@@ -1,12 +1,31 @@
+import uuid
 from uuid import UUID
 
 from sqlalchemy import Engine, select
 from sqlalchemy.orm import Session
 
 from papermerge.core import schemas
-from papermerge.core.db.models import ColoredTag, Document, DocumentVersion, Page
+from papermerge.core.db.models import (
+    ColoredTag,
+    CustomField,
+    CustomFieldValue,
+    Document,
+    DocumentVersion,
+    Page,
+)
 
 from .common import get_ancestors
+
+CUSTOM_FIELD_DATA_TYPE_MAP = {
+    "string": "text",
+    "boolean": "bool",
+    "url": "url",
+    "date": "date",
+    "integer": "int",
+    "float": "float",
+    "monetary": "monetary",
+    "select": "select",
+}
 
 
 def get_doc(
@@ -62,11 +81,30 @@ def get_doc(
 
 def update_document_custom_field_values(
     session: Session,
-    id: UUID,
+    id: UUID,  # id of the document
     custom_fields_update: schemas.DocumentCustomFieldsUpdate,
     user_id: UUID,
 ):
-    pass
+    custom_field_ids = [cf.id for cf in custom_fields_update.custom_fields]
+    stmt = select(CustomField).where(CustomField.id.in_(custom_field_ids))
+    custom_fields = [
+        schemas.CustomField.model_validate(cf) for cf in session.execute(stmt).all()
+    ]
+
+    for incoming_cf in custom_fields_update.custom_fields:
+        found = next((cf for cf in custom_fields if cf.id == incoming_cf.id), None)
+        if found:
+            cfv = CustomFieldValue(
+                id=uuid.uuid4(),
+                field_id=incoming_cf.custom_field_id,
+                document_id=id,
+            )
+            attr_name = CUSTOM_FIELD_DATA_TYPE_MAP.get(found.data_type, None)
+            if attr_name:
+                cfv[f"value_{attr_name}"] = incoming_cf.value
+                session.add(cfv)
+
+    session.commit()
 
 
 def get_document_custom_field_values(

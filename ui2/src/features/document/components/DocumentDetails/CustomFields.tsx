eb30c71e192956ef2d7dbd491985f15f5d5c4f0c
Eugen Ciur <eugen@papermerge.com>
2024-10-27 07:36:25 +0100

    error hndl for cf

--- ui2/src/features/document/components/DocumentDetails/CustomFields.tsx
+++ ui2/src/features/document/components/DocumentDetails/CustomFields.tsx
@@ -17,7 +17,14 @@ import {
 } from "@/features/document/apiSlice"
 import {selectCurrentNodeID} from "@/features/ui/uiSlice"
 import type {CFV, PanelMode} from "@/types"
-import {Button, ComboboxItem, Select, Skeleton, TextInput} from "@mantine/core"
+import {
+  Button,
+  ComboboxItem,
+  Select,
+  Skeleton,
+  Text,
+  TextInput
+} from "@mantine/core"
 
 export default function CustomFields() {
   const mode: PanelMode = useContext(PanelContext)
@@ -35,8 +42,11 @@ export default function CustomFields() {
   const [updateDocumentCustomFields, {error}] =
     useUpdateDocumentCustomFieldsMutation()
   const [updateDocumentType] = useUpdateDocumentTypeMutation()
-  const {data: documentCustomFields, isSuccess: isSuccessDocumentCustomFields} =
-    useGetDocumentCustomFieldsQuery(docID ?? skipToken)
+  const {
+    data: documentCustomFields,
+    isSuccess: isSuccessDocumentCustomFields,
+    isError: isErrorGetDocCF
+  } = useGetDocumentCustomFieldsQuery(docID ?? skipToken)
 
   useEffect(() => {
     if (documentCustomFields && documentCustomFields.length > 0) {
@@ -172,6 +182,11 @@ export default function CustomFields() {
         clearable
       />
       {genericCustomFieldsComponents}
+      {isErrorGetDocCF && (
+        <Text c="red">
+          Error while fetching custom fields. Error code cf98g62m.
+        </Text>
+      )}
       {showSaveButton && <Button onClick={onSave}>Save</Button>}
       {error && (
         <div>

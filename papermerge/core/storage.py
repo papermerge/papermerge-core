2e22b4ef292daae17395ab858163e0f1ef372322
Eugen Ciur <eugen@papermerge.com>
2024-10-31 09:15:11 +0100

    add default values for media root

--- papermerge/core/storage.py
+++ papermerge/core/storage.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 logger = logging.getLogger(__name__)
-MEDIA_ROOT = os.environ.get("PAPERMERGE__MAIN__MEDIA_ROOT", None)
+MEDIA_ROOT = os.environ.get("PAPERMERGE__MAIN__MEDIA_ROOT", "./media")
 
 
 def get_storage_class(import_path=None):
@@ -15,8 +15,4 @@ def get_storage_instance():
 
 
 def abs_path(some_relative_path: Path) -> Path:
-    MADIA_ROOT = os.environ.get("PAPERMERGE__MAIN__MEDIA_ROOT", None)
-    if MADIA_ROOT is None:
-        raise ValueError("PAPERMERGE__MAIN__MEDIA_ROOT is missing")
-
     return Path(MEDIA_ROOT) / some_relative_path

c77d000fae126e1d7d4307bb7cf276c662469dfe
Eugen Ciur <eugen@papermerge.com>
2024-11-02 10:07:12 +0100

    remove obsolete modules

--- papermerge/core/signals.py
+++ papermerge/core/signals.py
@@ -2,16 +2,13 @@ import logging
 from pathlib import Path
 
 from celery.app import default_app as celery_app
-from celery.signals import (heartbeat_sent, task_postrun, task_prerun,
-                            task_received, worker_ready, worker_shutdown)
+from celery.signals import (heartbeat_sent, worker_ready, worker_shutdown)
 from django.conf import settings
 from django.db.models.signals import post_delete, post_save, pre_delete
 from django.dispatch import receiver
 
 from papermerge.core import constants as const
 from papermerge.core.models import Document, DocumentVersion, Page, User
-from papermerge.core.notif import (Event, EventName, OCREvent, State,
-                                   notification)
 from papermerge.core.storage import get_storage_instance
 
 from .signal_definitions import document_post_upload
@@ -28,111 +25,6 @@ READINESS_FILE = Path("/tmp/worker_ready")
 MONITORED_TASKS = (
     'papermerge.core.tasks.ocr_document_task',
 )
-
-MONITORED_TASKS_KWARGS_TYPE_DICT = {
-    'papermerge.core.tasks.ocr_document_task': OCREvent
-}
-
-
-def update_document_ocr_status(event: Event) -> None:
-    if not event:
-        return
-
-    if event.name != EventName.ocr_document:
-        return
-
-    document_id = event.kwargs.document_id
-    ocr_status = event.state
-    logger.debug(f"Updating document {document_id} ocr_status={ocr_status}")
-
-    try:
-        document = Document.objects.get(pk=document_id)
-        document.ocr_status = ocr_status
-        document.save()
-    except Document.DoesNotExist as exc:
-        # not end of the world, but still good to know
-        logger.warning(
-            f"Consumer did not found the document_id={document_id}"
-        )
-        logger.warning(exc)
-        # life goes on...
-
-
-def channel_group_notify(
-    full_name: str,
-    state: State,
-    **kwargs
-) -> Event | None:
-    """
-    Send group notification to the channel
-    """
-    logger.debug("channel_group_notify")
-    if full_name in MONITORED_TASKS:
-        logger.debug(
-            f"full_name={full_name} state={state}"
-            f" kwargs={kwargs}"
-        )
-        logger.debug("Before creation of the Event")
-        # Validation failures WON'T BE REPORTED in
-        # ``papermerge.core`` logger !!!
-        # Validation failures are reported only in ``root``
-        # logger
-        event = Event(
-            name=full_name.split('.')[-1],
-            state=state,
-            kwargs=kwargs['kwargs']
-        )
-        logger.debug(f"Before pushing the event: {event}")
-        notification.push(event)
-        logger.debug(f"After  pushing the event: {event}")
-
-        return event
-
-
-@task_prerun.connect
-def channel_group_notify_task_prerun(sender=None, **kwargs):
-    if not sender:
-        return
-
-    event = channel_group_notify(
-        full_name=sender.name,
-        state=State.started,
-        kwargs=kwargs['kwargs']
-    )
-
-    update_document_ocr_status(event)
-
-
-@task_postrun.connect
-def channel_group_notify_task_postrun(sender=None, **kwargs):
-    if not sender:
-        return
-
-    event = channel_group_notify(
-        full_name=sender.name,
-        state=kwargs['state'],
-        kwargs=kwargs['kwargs']
-    )
-
-    update_document_ocr_status(event)
-
-
-@task_received.connect
-def channel_group_notify_task_received(sender=None, **kwargs):
-    # why here is ``requests`` instead of ``sender``?
-    logger.debug(f"Task notification received kwargs={kwargs}")
-    request = kwargs.get('request')
-    if not request:
-        logger.debug("Empty request. Bye Bye.")
-        return
-
-    event = channel_group_notify(
-        full_name=request.name,
-        state=State.received,
-        kwargs=request.kwargs
-    )
-
-    update_document_ocr_status(event)
 
 
 @receiver(pre_delete, sender=Document)

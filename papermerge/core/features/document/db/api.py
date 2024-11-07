bc0692a7bd3fb43f67ce809967813929f80c2b13
Eugen Ciur <eugen@papermerge.com>
2024-11-07 17:00:52 +0100

    fix failing test

--- papermerge/core/features/document/db/api.py
+++ papermerge/core/features/document/db/api.py
@@ -791,43 +791,13 @@ def get_doc(
     stmt_doc = select(orm.Document).where(
         orm.Document.id == id, orm.Document.user_id == user_id
     )
-    db_doc = session.scalars(stmt_doc).one()
+    db_doc = session.scalar(stmt_doc)
     breadcrumb = get_ancestors(session, id)
     db_doc.breadcrumb = breadcrumb
 
-    stmt_doc_ver = (
-        select(orm.DocumentVersion)
-        .where(
-            orm.DocumentVersion.document_id == id,
-        )
-        .order_by("number")
-    )
-    db_doc_vers = session.scalars(stmt_doc_ver).all()
-
-    stmt_pages = select(orm.Page).where(orm.Document.id == id)
-    db_pages = session.scalars(stmt_pages).all()
-
-    db_doc.versions = list(
-        [
-            schema.DocumentVersion.model_validate(db_doc_ver)
-            for db_doc_ver in db_doc_vers
-        ]
-    )
-    # colored_tags_stmt = select(Tag).where(Tag.node_id == id)
     # colored_tags = session.scalars(colored_tags_stmt).all()
     # db_doc.tags = [ct.tag for ct in colored_tags]
 
-    def get_page(doc_ver_id):
-        result = []
-        for db_page in db_pages:
-            if db_page.document_version_id == doc_ver_id:
-                result.append(db_page)
-
-        return sorted(result, key=lambda x: x.number)
-
-    for version in db_doc.versions:
-        pages = get_page(version.id)
-        version.pages = list([schema.Page.model_validate(page) for page in pages])
     model_doc = schema.Document.model_validate(db_doc)
 
     return model_doc

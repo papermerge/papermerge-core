192f17f5b4087ef4bd83c8ebb6ad69b5c238fc08
Eugen Ciur <eugen@papermerge.com>
2024-11-07 15:52:25 +0100

    fix get tag details plus test

--- papermerge/core/schema.py
+++ papermerge/core/schema.py
@@ -1,6 +1,19 @@
 from .features.nodes.schema import Folder, NewFolder, Node
 from .features.document.schema import Document, DocumentVersion, Page
 from .features.users.schema import User
-from .features.tags.schema import Tag, UpdateTag
+from .features.tags.schema import Tag, UpdateTag, CreateTag
+from .schemas.error import Error
 
-__all__ = ['Folder', 'NewFolder', 'Node', 'Document', 'DocumentVersion', 'Page', 'User', 'Tag', 'UpdateTag']
+__all__ = [
+    'Folder',
+    'NewFolder',
+    'Node',
+    'Document',
+    'DocumentVersion',
+    'Page',
+    'User',
+    'Tag',
+    'UpdateTag',
+    'CreateTag',
+    'Error'
+]

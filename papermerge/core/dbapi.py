3ba09e7b3e657eb1ff6449a4681f1ed6cd862023
Eugen Ciur <eugen@papermerge.com>
2024-11-11 12:03:03 +0100

    fix get-doc-cfv method

--- papermerge/core/dbapi.py
+++ papermerge/core/dbapi.py
@@ -6,7 +6,8 @@ from .features.document.db.api import (
     get_doc,
     get_docs_by_type,
     get_docs_count_by_type,
-    update_doc_type
+    update_doc_type,
+    get_doc_cfv
 )
 from .features.document_types.db.api import (
     create_document_type,
@@ -23,6 +24,7 @@ __all__ = [
     "get_last_doc_ver",
     "get_doc_ver",
     "get_doc",
+    "get_doc_cfv",
     "get_docs_by_type",
     "update_doc_type",
     "get_docs_count_by_type",

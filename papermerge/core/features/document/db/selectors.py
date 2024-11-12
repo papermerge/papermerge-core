b7ef70fad9d2599dc09eea4c0abdbb55a28071ae
Eugen Ciur <eugen@papermerge.com>
2024-11-12 08:16:09 +0100

    minor refactoring

--- papermerge/core/features/document/db/selectors.py
+++ papermerge/core/features/document/db/selectors.py
@@ -1,6 +1,7 @@
 import uuid
 
 from sqlalchemy import select, Select, case, func, VARCHAR
+from sqlalchemy.orm import aliased
 
 from papermerge.core import orm
 
@@ -24,29 +25,33 @@ def select_doc_cf(document_id: uuid.UUID) -> Select:
 def select_doc_cfv(document_id: uuid.UUID) -> Select:
     """Returns SqlAlchemy selector for document custom field values"""
     cf = select_doc_cf(document_id).subquery("cf")
+    cfv = aliased(orm.CustomFieldValue, name="cfv")
+    assoc = aliased(orm.DocumentTypeCustomField, name="assoc")
+    doc = aliased(orm.Document, name="doc")
+
     stmt = select(
-        orm.Document.id.label("doc_id"),
-        orm.Document.document_type_id,
+        doc.id.label("doc_id"),
+        doc.document_type_id,
         cf.c.name.label("cf_name"),
         cf.c.extra_data.label("cf_extra_data"),
         cf.c.type.label("cf_type"),
         cf.c.id.label("cf_id"),
-        orm.CustomFieldValue.id.label("cfv_id"),
+        cfv.id.label("cfv_id"),
         case(
-            (cf.c.type == 'monetary', func.cast(orm.CustomFieldValue.value_monetary, VARCHAR)),
-            (cf.c.type == 'text', func.cast(orm.CustomFieldValue.value_text, VARCHAR)),
-            (cf.c.type == 'date', func.substr(func.cast(orm.CustomFieldValue.value_date, VARCHAR), 0, 11)),
-            (cf.c.type == 'boolean', func.cast(orm.CustomFieldValue.value_boolean, VARCHAR)),
+            (cf.c.type == 'monetary', func.cast(cfv.value_monetary, VARCHAR)),
+            (cf.c.type == 'text', func.cast(cfv.value_text, VARCHAR)),
+            (cf.c.type == 'date', func.substr(func.cast(cfv.value_date, VARCHAR), 0, 11)),
+            (cf.c.type == 'boolean', func.cast(cfv.value_boolean, VARCHAR)),
         ).label("cf_value")
-    ).select_from(orm.Document).join(
-        orm.DocumentTypeCustomField,
-        orm.DocumentTypeCustomField.document_type_id == orm.Document.document_type_id
+    ).select_from(doc).join(
+        assoc,
+        assoc.document_type_id == doc.document_type_id
     ).join(
         cf,
-        cf.c.id == orm.DocumentTypeCustomField.custom_field_id
+        cf.c.id == assoc.custom_field_id
     ).join(
-        orm.CustomFieldValue,
-        orm.CustomFieldValue.field_id == cf.c.id and orm.CustomFieldValue.document_id == document_id,
+        cfv,
+        cfv.field_id == cf.c.id and cfv.document_id == document_id,
         isouter=True
     )
 

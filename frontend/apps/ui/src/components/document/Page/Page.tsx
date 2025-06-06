fb1a5140d8011a12a67fe2ed5e78042289c25f45
Eugen Ciur <ciur.eugen@gmail.com>
2025-06-06 07:37:49 +0200

    paginated document viewer

--- frontend/apps/ui/src/components/document/Page/Page.tsx
+++ frontend/apps/ui/src/components/document/Page/Page.tsx
@@ -1,39 +1,40 @@
-import {useAppSelector} from "@/app/hooks"
+import { useAppSelector } from "@/app/hooks"
 import PanelContext from "@/contexts/PanelContext"
-import {selectBestImageByPageId} from "@/features/document/selectors"
+import { selectBestImageByPageId } from "@/features/document/selectors"
 import {
   selectDocumentCurrentPage,
   selectZoomFactor
 } from "@/features/ui/uiSlice"
-import {ClientPage, PanelMode} from "@/types"
-import {Skeleton, Stack} from "@mantine/core"
-import {useContext, useEffect, useRef} from "react"
+import { PanelMode } from "@/types"
+import { Skeleton, Stack } from "@mantine/core"
+import { useContext, useEffect, useRef } from "react"
 
 import classes from "./Page.module.css"
-type Args = {
-  page: ClientPage
+interface Args {
+  page_number: number
+  page_id: string
 }
 
-export default function Page({page}: Args) {
+export default function Page({page_number, page_id}: Args) {
   const mode: PanelMode = useContext(PanelContext)
   const currentPage = useAppSelector(s => selectDocumentCurrentPage(s, mode))
   const targetRef = useRef<HTMLImageElement | null>(null)
   const zoomFactor = useAppSelector(s => selectZoomFactor(s, mode))
-  const bestImageURL = useAppSelector(s => selectBestImageByPageId(s, page.id))
+  const bestImageURL = useAppSelector(s => selectBestImageByPageId(s, page_id))
 
   useEffect(() => {
-    if (currentPage == page.number) {
+    if (currentPage == page_number) {
       if (targetRef.current) {
         targetRef.current.scrollIntoView(false)
       }
     }
-  }, [currentPage, bestImageURL, page.number])
+  }, [currentPage, bestImageURL, page_number])
 
   if (!bestImageURL) {
     return (
       <Stack justify="center" align="center">
         <Skeleton width={"80%"} height={800} />
-        <div>{page.number}</div>
+        <div>{page_number}</div>
       </Stack>
     )
   }
@@ -41,11 +42,11 @@ export default function Page({page}: Args) {
   return (
     <Stack className={classes.page}>
       <img
-        style={{transform: `rotate(${page.angle}deg)`, width: `${zoomFactor}%`}}
+        /* style={{transform: `rotate(${page.angle}deg)`, width: `${zoomFactor}%`}} */
         ref={targetRef}
         src={bestImageURL}
       />
-      <div>{page.number}</div>
+      <div>{page_number}</div>
     </Stack>
   )
 }

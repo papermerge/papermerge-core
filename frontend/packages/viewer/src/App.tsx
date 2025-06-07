d72d2ef51804f24c3aec54da2a9fca9af958edd2
Eugen Ciur <ciur.eugen@gmail.com>
2025-06-07 08:44:43 +0200

    remove unused files

--- frontend/packages/viewer/src/App.tsx
+++ frontend/packages/viewer/src/App.tsx
@@ -1,4 +1,4 @@
-import {MantineProvider} from "@mantine/core"
+import {Checkbox, Group, MantineProvider, Stack} from "@mantine/core"
 import page_a_md from "./assets/pages/page_a/md.jpg"
 import Page from "./components/Page"
 
@@ -6,7 +6,12 @@ function App() {
   return (
     <>
       <MantineProvider>
-        <Page pageNumber={1} imageURL={page_a_md} isLoading={false} />
+        <Stack>
+          <Group>
+            <Checkbox label="Is Loading" />
+          </Group>
+          <Page pageNumber={1} imageURL={page_a_md} isLoading={false} />
+        </Stack>
       </MantineProvider>
     </>
   )

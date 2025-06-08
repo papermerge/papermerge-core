082d9f85d77ef65abd436d943c5a5628e4691142
Eugen Ciur <ciur.eugen@gmail.com>
2025-06-08 08:10:46 +0200

    refactor page list component

---
+++ frontend/apps/ui/src/components/document/PageList/PageList.tsx
@@ -0,0 +1,23 @@
+import {Page} from "@papermerge/viewer"
+import page_a_md from "../assets/pages/page_a/md.jpg"
+
+type PageStruct = {
+  id: string
+  number: number
+}
+
+interface Args {
+  pages: Array<PageStruct>
+}
+
+export default function Pages({pages}: Args) {
+  const pagesComponent = pages.map(p => (
+    <Page
+      key={p.id}
+      pageNumber={p.number}
+      isLoading={false}
+      imageURL={page_a_md}
+    />
+  ))
+  return <>{pagesComponent}</>
+}

82a870624f5ff2132f604bb6707c0ceabd5ecaed
Eugen Ciur <ciur.eugen@gmail.com>
2025-06-13 06:39:26 +0200

    adjust couple of tests

--- papermerge/core/features/document/tests/test_document_version_router.py
+++ papermerge/core/features/document/tests/test_document_version_router.py
@@ -5,7 +5,6 @@ from pathlib import Path
 from papermerge.core import dbapi
 from papermerge.core.tests.resource_file import ResourceFile
 
-
 DIR_ABS_PATH = os.path.abspath(os.path.dirname(__file__))
 RESOURCES = Path(DIR_ABS_PATH) / "resources"
 
@@ -28,10 +27,26 @@ def test_document_version_download_request_non_existing_resource(auth_api_client
     response = auth_api_client.get(
         f"/document-versions/{non_existing_resource_id}/download"
     )
-    assert response.status_code == 404
+    assert response.status_code == 403
 
 
 def test_document_version_details_request_non_existing_resource(auth_api_client):
     non_existing_resource_id = uuid.uuid4().hex
     response = auth_api_client.get(f"/document-versions/{non_existing_resource_id}")
-    assert response.status_code == 404
+    assert response.status_code == 403
+
+
+def test_get_doc_ver_download_url(
+    auth_api_client, make_document_from_resource, user, db_session
+):
+    doc = make_document_from_resource(
+        resource=ResourceFile.THREE_PAGES, user=user, parent=user.home_folder
+    )
+
+    last_ver = dbapi.get_last_doc_ver(db_session, doc_id=doc.id)
+
+    response = auth_api_client.get(f"/document-versions/{last_ver.id}/download-url")
+
+    assert response.status_code == 200
+    download_url = response.json()
+    assert str(last_ver.id) in download_url

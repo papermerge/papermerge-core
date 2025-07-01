cbc2d9900f0a4b25dfe3d1fc4d683fbae2c290b9
Eugen Ciur <ciur.eugen@gmail.com>
2025-07-01 08:22:58 +0200

    minor adjustments

--- frontend/apps/ui/src/features/document/actions/applyPageOpChanges.ts
+++ frontend/apps/ui/src/features/document/actions/applyPageOpChanges.ts
@@ -12,60 +12,55 @@ export const applyPageChangesThunk = createAsyncThunk<
   void, // return type
   {docID: string; pages: ClientPage[]; mode: PanelMode}, // argument type
   {state: RootState} // thunkAPI config
->(
-  "document/applyPageChanges",
-  async ({docID, pages, mode}, {getState, dispatch}) => {
-    const state = getState()
-    const pageData = pages.map(p => {
-      const result = {
-        angle: p.angle,
-        page: {
-          number: p.number,
-          id: p.id
-        }
+>("document/applyPageChanges", async ({docID, pages, mode}, {dispatch}) => {
+  const pageData = pages.map(p => {
+    const result = {
+      angle: p.angle,
+      page: {
+        number: p.number,
+        id: p.id
       }
-      return result
-    })
-
-    // Call RTK Query mutation manually
-    const result = await dispatch(
-      apiSliceWithDocuments.endpoints.applyPageOpChanges.initiate({
-        pages: pageData,
-        documentID: docID
-      })
-    )
-
-    if ("error" in result) {
-      throw result.error // propagates error to rejected action
     }
+    return result
+  })
 
-    if (!result.data) {
-      throw "No data"
-    }
-
-    let newDoc = result.data
-    let lastVersion = getLastVersion(newDoc.versions)
-
-    const updates = lastVersion.pages.map(lastVerPage => {
-      const oldInfo = pageData[lastVerPage.number - 1]
-      return {
-        newPageID: lastVerPage.id,
-        oldPageID: oldInfo.page.id,
-        angle: oldInfo.angle,
-        docVerID: lastVersion.id,
-        docID: lastVersion.document_id,
-        number: lastVersion.number
-      }
+  const result = await dispatch(
+    apiSliceWithDocuments.endpoints.applyPageOpChanges.initiate({
+      pages: pageData,
+      documentID: docID
     })
+  )
 
-    dispatch(addImageObjectsFromPrevious({updates}))
-
-    let ver = clientDVFromDV(lastVersion)
-
-    dispatch(addClientDocVersion(ver))
-    dispatch(currentDocVerUpdated({mode: mode, docVerID: lastVersion.id}))
+  if ("error" in result) {
+    throw result.error
   }
-)
+
+  if (!result.data) {
+    throw "No data"
+  }
+
+  let newDoc = result.data
+  let lastVersion = getLastVersion(newDoc.versions)
+
+  const updates = lastVersion.pages.map(lastVerPage => {
+    const oldInfo = pageData[lastVerPage.number - 1]
+    return {
+      newPageID: lastVerPage.id,
+      oldPageID: oldInfo.page.id,
+      angle: oldInfo.angle,
+      docVerID: lastVersion.id,
+      docID: lastVersion.document_id,
+      number: lastVersion.number
+    }
+  })
+
+  dispatch(addImageObjectsFromPrevious({updates}))
+
+  let ver = clientDVFromDV(lastVersion)
+
+  dispatch(addClientDocVersion(ver))
+  dispatch(currentDocVerUpdated({mode: mode, docVerID: lastVersion.id}))
+})
 
 function getLastVersion(docVers: DocumentVersion[]): DocumentVersion {
   if (docVers.length === 0) {

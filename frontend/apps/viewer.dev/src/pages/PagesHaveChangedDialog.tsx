6d4949411ec91677fff0ab80f440a803bc69ed0a
Eugen Ciur <ciur.eugen@gmail.com>
2025-07-02 08:34:20 +0200

    fix component layout for german i18n

--- frontend/apps/viewer.dev/src/pages/PagesHaveChangedDialog.tsx
+++ frontend/apps/viewer.dev/src/pages/PagesHaveChangedDialog.tsx
@@ -1,10 +1,22 @@
-import {Checkbox, Divider, Group, Stack} from "@mantine/core"
-import {useState} from "react"
+import {
+  Checkbox,
+  ComboboxItem,
+  Divider,
+  Group,
+  Select,
+  Stack
+} from "@mantine/core"
+import {useEffect, useState} from "react"
+import type {I18NPagesHaveChangedDialogText} from "viewer"
 import {PagesHaveChangedDialog} from "viewer"
+
+type Lang = "german" | "english"
 
 export default function PagesHaveChangedDialogContainer() {
   const [inProgress, setInProgress] = useState<boolean>(false)
   const [opened, setOpened] = useState<boolean>(false)
+  const [lang, setLang] = useState<Lang>("english")
+  const [txt, setTxt] = useState<I18NPagesHaveChangedDialogText>()
 
   const InProgressClicked = () => {
     setInProgress(!inProgress)
@@ -14,15 +26,51 @@ export default function PagesHaveChangedDialogContainer() {
     setOpened(!opened)
   }
 
+  const onSelect = (_value: string | null, option: ComboboxItem | null) => {
+    if (option?.value) {
+      setLang(option.value as Lang)
+    }
+  }
+
+  useEffect(() => {
+    if (lang == "german") {
+      setTxt({
+        pagesHaveChanged:
+          "Es gibt Änderungen, wie Reihenfolge oder Drehung der Seiten, die noch nicht auf dem Server gespeichert sind. Was möchtest du tun?",
+        save: "Speichern",
+        reset: "Zurücksetzen",
+        saveTooltip:
+          "Änderungen auf den Server übernehmen. Nach dem Übernehmen erhöht sich die Dokumentversion um eins",
+        resetTooltip:
+          "Änderungen verwerfen. Dadurch werden die Seiten in ihren ursprünglichen Zustand zurückgesetzt (wie auf dem Server)",
+        dontBotherMe: "Lass mich in Ruhe",
+        dontBotherMeTooltip:
+          "Diesen Modal schließen. Änderungen kannst du später über das Kontextmenü übernehmen"
+      })
+    } else {
+      setTxt(undefined)
+    }
+  }, [lang])
+
   return (
     <Stack>
       <Group>
         <Checkbox label="In Progress" onClick={InProgressClicked} />
         <Checkbox label="Opened" onClick={OpenedClicked} />
+        <Select
+          label="Versions"
+          placeholder="Pick a value"
+          data={["english", "german"]}
+          onChange={onSelect}
+        />
       </Group>
       <Divider />
       <Group>
-        <PagesHaveChangedDialog opened={opened} inProgress={inProgress} />
+        <PagesHaveChangedDialog
+          opened={opened}
+          inProgress={inProgress}
+          txt={txt}
+        />
       </Group>
     </Stack>
   )

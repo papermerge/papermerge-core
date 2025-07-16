94ce767c18499319a4e6572a6ccb3c06765bbcc6
Eugen Ciur <eugen@papermerge.com>
2025-07-16 05:35:57 +0200

    do not send user id to template worker (#683)

--- papermerge/core/features/document/router.py
+++ papermerge/core/features/document/router.py
@@ -86,7 +86,7 @@ def update_document_custom_field_values(
 
     send_task(
         const.PATH_TMPL_MOVE_DOCUMENT,
-        kwargs={"document_id": str(document_id), "user_id": str(user.id)},
+        kwargs={"document_id": str(document_id)},
         route_name="path_tmpl",
     )
 

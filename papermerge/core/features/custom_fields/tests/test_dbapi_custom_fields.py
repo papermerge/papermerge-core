ff35f57c1a14710b617d752a0ccab1ef76290f35
Eugen Ciur <ciur.eugen@gmail.com>
2025-07-26 10:01:46 +0200

    6 tests passed!

--- papermerge/core/features/custom_fields/tests/test_dbapi_custom_fields.py
+++ papermerge/core/features/custom_fields/tests/test_dbapi_custom_fields.py
@@ -1,12 +1,12 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from papermerge.core.features.custom_fields import schema
 from papermerge.core.features.custom_fields.db import api as dbapi
 from papermerge.core.features.users.db.orm import User
 
 
-def test_custom_filed_create(db_session: Session, user: User):
-    cfield = dbapi.create_custom_field(
+async def test_custom_filed_create(db_session: AsyncSession, user: User):
+    cfield = await dbapi.create_custom_field(
         db_session,
         name="cf1",
         type=schema.CustomFieldType.text,
@@ -16,37 +16,37 @@ def test_custom_filed_create(db_session: Session, user: User):
     assert cfield.name == "cf1"
     assert cfield.type == schema.CustomFieldType.text
 
-    retrieved_cf1 = dbapi.get_custom_field(db_session, cfield.id)
+    retrieved_cf1 = await dbapi.get_custom_field(db_session, cfield.id)
     assert retrieved_cf1.id == cfield.id
     assert retrieved_cf1.name == cfield.name
 
 
-def test_custom_field_delete(db_session: Session, user: User):
-    cfield = dbapi.create_custom_field(
+async def test_custom_field_delete(db_session: AsyncSession, user: User):
+    cfield = await dbapi.create_custom_field(
         db_session,
         name="cf1",
         type=schema.CustomFieldType.text,
         user_id=user.id,
     )
 
-    retrieved_cf1 = dbapi.get_custom_field(db_session, cfield.id)
+    retrieved_cf1 = await dbapi.get_custom_field(db_session, cfield.id)
     assert retrieved_cf1.id == cfield.id
     assert retrieved_cf1.name == cfield.name
 
 
-def test_custom_field_update(db_session: Session, user: User):
-    cfield = dbapi.create_custom_field(
+async def test_custom_field_update(db_session: AsyncSession, user: User):
+    cfield = await dbapi.create_custom_field(
         db_session,
         name="cf1",
         type=schema.CustomFieldType.text,
         user_id=user.id,
     )
 
-    dbapi.update_custom_field(
+    await dbapi.update_custom_field(
         db_session,
         custom_field_id=cfield.id,
         attrs=schema.UpdateCustomField(name="new_cf1_name", user_id=user.id),
     )
 
-    updated_cf1 = dbapi.get_custom_field(db_session, cfield.id)
+    updated_cf1 = await dbapi.get_custom_field(db_session, cfield.id)
     assert updated_cf1.name == "new_cf1_name"

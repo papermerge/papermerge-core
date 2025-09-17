dfdcf8342a81bbe3f747270fc878e1c06958cd00
Eugen Ciur <ciur.eugen@gmail.com>
2025-09-17 08:03:27 +0200

    add column selector

--- frontend/apps/ui/src/features/users/components/ActionButtons.tsx
+++ frontend/apps/ui/src/features/users/components/ActionButtons.tsx
@@ -4,6 +4,7 @@ import {Group} from "@mantine/core"
 
 import {usePanelMode} from "@/hooks"
 import ChangePasswordButton from "./ChangePasswordButton"
+import ColumnSelector from "./ColumnSelector"
 import {DeleteUsersButton} from "./DeleteButton"
 import EditButton from "./EditButton"
 import NewButton from "./NewButton"
@@ -13,15 +14,20 @@ export default function ActionButtons() {
   const selectedIds = useAppSelector(s => selectSelectedIDs(s, mode)) || []
 
   return (
-    <Group>
-      <NewButton />
-      {selectedIds?.length == 1 ? (
-        <ChangePasswordButton userId={selectedIds[0]} />
-      ) : (
-        ""
-      )}
-      {selectedIds?.length == 1 ? <EditButton userId={selectedIds[0]} /> : ""}
-      {selectedIds.length >= 1 ? <DeleteUsersButton /> : ""}
+    <Group justify="space-between" w={"100%"}>
+      <Group>
+        <NewButton />
+        {selectedIds?.length == 1 ? (
+          <ChangePasswordButton userId={selectedIds[0]} />
+        ) : (
+          ""
+        )}
+        {selectedIds?.length == 1 ? <EditButton userId={selectedIds[0]} /> : ""}
+        {selectedIds.length >= 1 ? <DeleteUsersButton /> : ""}
+      </Group>
+      <Group>
+        <ColumnSelector />
+      </Group>
     </Group>
   )
 }

64f0c474b642e0c8797213ff0a8ad9008cc336a0
Eugen Ciur <ciur.eugen@gmail.com>
2025-09-17 07:44:28 +0200

    Make <NewButton /> reusable

--- frontend/apps/ui/src/features/roles/components/NewButton.tsx
+++ frontend/apps/ui/src/features/roles/components/NewButton.tsx
@@ -1,23 +1,18 @@
-import {Button} from "@mantine/core"
+import NewButton from "@/components/NewButton"
 import {useDisclosure} from "@mantine/hooks"
-import {IconPlus} from "@tabler/icons-react"
 import {useTranslation} from "react-i18next"
 import NewRoleModal from "./NewRoleModal"
 
-export default function NewButton() {
+export default function NewButtonContainer() {
   const {t} = useTranslation()
   const [opened, {open, close}] = useDisclosure(false)
 
   return (
     <>
-      <Button
-        leftSection={<IconPlus />}
+      <NewButton
         onClick={open}
-        color={"teal"}
-        variant="filled"
-      >
-        {t("roles.create_new_role")}
-      </Button>
+        text={t("roles.addNewRole", {defaultValue: "Add"})}
+      />
       <NewRoleModal opened={opened} onSubmit={close} onCancel={close} />
     </>
   )

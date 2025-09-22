66956f6852b06714b357a49ac62f587c8cce4700
Eugen Ciur <ciur.eugen@gmail.com>
2025-09-22 07:47:35 +0200

    lil bit of translations

--- frontend/apps/ui/src/features/groups/components/NewButton.tsx
+++ frontend/apps/ui/src/features/groups/components/NewButton.tsx
@@ -1,18 +1,19 @@
-import {Button} from "@mantine/core"
-import {IconPlus} from "@tabler/icons-react"
+import NewButton from "@/components/NewButton"
 import {useDisclosure} from "@mantine/hooks"
-import NewGroupModal from "./NewGroupModal"
 import {useTranslation} from "react-i18next"
+import NewGroupModal from "./NewGroupModal"
 
-export default function NewButton() {
+export default function NewButtonContainer() {
   const {t} = useTranslation()
   const [opened, {open, close}] = useDisclosure(false)
 
   return (
     <>
-      <Button leftSection={<IconPlus />} onClick={open} variant="default">
-        {t("common.new")}
-      </Button>
+      <NewButton
+        onClick={open}
+        text={t("users.addNewUser", {defaultValue: "Add"})}
+      />
+
       <NewGroupModal opened={opened} onSubmit={close} onCancel={close} />
     </>
   )

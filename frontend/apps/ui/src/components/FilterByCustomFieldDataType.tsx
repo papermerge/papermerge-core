9abafac660063783c07651e739a6f9633ce4eee4
Eugen Ciur <ciur.eugen@gmail.com>
2025-09-27 07:07:26 +0200

    little bit of localization

--- frontend/apps/ui/src/components/FilterByCustomFieldDataType.tsx
+++ frontend/apps/ui/src/components/FilterByCustomFieldDataType.tsx
@@ -1,3 +1,4 @@
+import {getCustomFieldTypes} from "@/features/custom-fields/utils"
 import {ComboboxItem, MultiSelect, Paper} from "@mantine/core"
 import {TFunction} from "i18next"
 
@@ -14,42 +15,7 @@ export default function FilterByCustomFieldDataType({
   t,
   label
 }: Args) {
-  const data: ComboboxItem[] = [
-    {
-      value: "date",
-      label: t?.("customFieldType.date.label", {defaultValue: "Date"}) || "Date"
-    },
-    {
-      value: "text",
-      label: t?.("customFieldType.text", {defaultValue: "Text"}) || "Text"
-    },
-    {
-      value: "boolean",
-      label:
-        t?.("customFieldType.boolean", {defaultValue: "Yes/No"}) || "Yes/No"
-    },
-    {
-      value: "int",
-      label: t?.("customFieldType.int", {defaultValue: "Integer"}) || "Integer"
-    },
-    {
-      value: "float",
-      label:
-        t?.("customFieldType.float", {defaultValue: "Decimal"}) || "Decimal"
-    },
-    {
-      value: "monetary",
-      label:
-        t?.("customFieldType.monetary", {defaultValue: "Currency"}) ||
-        "Currency"
-    },
-    {
-      value: "yearmonth",
-      label:
-        t?.("customFieldType.yearmonth", {defaultValue: "Year/Month"}) ||
-        "Year/Month"
-    }
-  ]
+  const data: ComboboxItem[] = getCustomFieldTypes(t)
 
   return (
     <Paper
--- frontend/apps/ui/src/features/custom-fields/components/CustomFieldForm.tsx
+++ frontend/apps/ui/src/features/custom-fields/components/CustomFieldForm.tsx
@@ -1,40 +1,42 @@
-import {CUSTOM_FIELD_DATA_TYPES} from "@/cconstants"
 import CopyButton from "@/components/CopyButton"
 import OwnerIcon from "@/components/OwnerIcon"
 import type {CustomFieldItem} from "@/features/custom-fields/types"
+import {getCustomFieldTypes} from "@/features/custom-fields/utils"
 import {Box, NativeSelect, TextInput} from "@mantine/core"
+import {TFunction} from "i18next"
 
 type Args = {
   customField: CustomFieldItem | null
+  t?: TFunction
 }
 
-export default function CustomFieldForm({customField}: Args) {
+export default function CustomFieldForm({customField, t}: Args) {
   return (
     <Box>
       <TextInput
         my="md"
-        label="ID"
+        label={t?.("customFieldColumns.id") || "ID"}
         value={customField?.id || ""}
         onChange={() => {}}
         rightSection={<CopyButton value={customField?.id || ""} />}
       />
       <TextInput
         my="md"
-        label="Name"
+        label={t?.("customFieldColumns.name") || "Name"}
         value={customField?.name || ""}
         onChange={() => {}}
         rightSection={<CopyButton value={customField?.name || ""} />}
       />
       <NativeSelect
         mt="sm"
-        label="Type"
+        label={t?.("customFieldColumns.type") || "Type"}
         value={customField?.type || ""}
-        data={CUSTOM_FIELD_DATA_TYPES}
+        data={getCustomFieldTypes(t)}
         onChange={() => {}}
       />
       <TextInput
         my="md"
-        label="Owner"
+        label={t?.("customFieldColumns.owned_by") || "Owned By"}
         value={customField?.owned_by.name || "Me"}
         onChange={() => {}}
         leftSection={<OwnerIcon owner={customField?.owned_by} />}

63f2d1d9b0dafbaed9d6358d7ca2b89a32bfd43e
Eugen Ciur <ciur.eugen@gmail.com>
2025-10-03 06:55:38 +0200

    remove comment

--- frontend/apps/ui/src/app/hooks/useApp.tsx
+++ frontend/apps/ui/src/app/hooks/useApp.tsx
@@ -1,7 +1,6 @@
 import {useDispatch, useSelector} from "react-redux"
 import type {AppDispatch, RootState} from "../types"
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
 const useAppDispatch = useDispatch.withTypes<AppDispatch>()
 const useAppSelector = useSelector.withTypes<RootState>()
 

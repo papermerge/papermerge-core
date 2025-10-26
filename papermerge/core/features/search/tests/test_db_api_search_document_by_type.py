86d0a82be53971e8c9dad84e417351ea2fad7352
Eugen Ciur <eugen@papermerge.com>
2025-10-26 11:17:56 +0100

    Add more tests for search docs by type (#1034)

--- papermerge/core/features/search/tests/test_db_api_search_document_by_type.py
+++ papermerge/core/features/search/tests/test_db_api_search_document_by_type.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from papermerge.core.features.search import schema as search_schema
 from papermerge.core.features.search.db import api as search_dbapi
 from papermerge.core.features.custom_fields.db import api as cf_dbapi
+from papermerge.core.features.document.db import api as doc_dbapi
+from collections import namedtuple
 
 
 async def test_fitler_document_numeric_custom_field(
@@ -85,3 +87,203 @@ async def test_fitler_document_numeric_custom_field(
     assert len(results.items) == 1
     assert results.items[0].document_id == doc2.id
     assert results.items[0].title == "tiny-bill.pdf"
+
+
+async def test_filter_documents_by_multiple_cf(
+    db_session: AsyncSession,
+    user,
+    make_document_type_with_custom_fields,
+    make_document
+):
+    doc_1 = await make_document(
+        title="doc_1.pdf",
+        user=user,
+        parent=user.home_folder,
+        lang=search_schema.SearchLanguage.ENG
+    )
+    doc_2 = await make_document(
+        title="doc_2.pdf",
+        user=user,
+        parent=user.home_folder,
+        lang=search_schema.SearchLanguage.ENG
+    )
+    doc_3 = await make_document(
+        title="doc_3.pdf",
+        user=user,
+        parent=user.home_folder,
+        lang=search_schema.SearchLanguage.ENG
+    )
+
+    invoice_type = await make_document_type_with_custom_fields(
+        name="Invoice",
+        custom_fields=[
+            {"name": "Invoice Date", "type_handler": "date"},
+            {"name": "Vendor", "type_handler": "text"},
+            {
+                "name": "Total Amount", "type_handler": "monetary",
+                "config": {"currency": "EUR"}
+            },
+        ]
+    )
+    for doc in [doc_1, doc_2, doc_3]:
+        await doc_dbapi.update_doc_type(
+            db_session,
+            document_id=doc.id,
+            document_type_id=invoice_type.id
+        )
+
+    await cf_dbapi.update_document_custom_field_values(
+        db_session,
+        document_id=doc_1.id,
+        custom_fields={
+            "Total Amount": "19.05",
+            "Vendor": "lidl",
+            "Invoice Date": "2024-12-15"
+        }
+    )
+    await cf_dbapi.update_document_custom_field_values(
+        db_session,
+        document_id=doc_2.id,
+        custom_fields={
+            "Total Amount": "59.05",
+            "Vendor": "lidl",
+            "Invoice Date": "2024-12-16"
+        }
+    )
+    await cf_dbapi.update_document_custom_field_values(
+        db_session,
+        document_id=doc_3.id,
+        custom_fields={
+            "Total Amount": "69.00",
+            "Vendor": "rewe",
+            "Invoice Date": "2024-12-17"
+        }
+    )
+
+    params = search_schema.SearchQueryParams(
+        filters=search_schema.SearchFilters(
+            custom_fields=[
+                search_schema.CustomFieldFilter(
+                    field_name="Total Amount",
+                    operator=search_schema.Operator.GT,
+                    value=30
+                ),
+                search_schema.CustomFieldFilter(
+                    field_name="Vendor",
+                    operator=search_schema.Operator.EQ,
+                    value="lidl"
+                )
+            ]
+        ),
+        lang=search_schema.SearchLanguage.ENG,
+        document_type_id=invoice_type.id,
+    )
+
+    # search for receipts with total amount > 30 and vendor = "lidl"
+    results = await search_dbapi.search_documents_by_type(
+        db_session=db_session,
+        user_id=user.id,
+        params=params
+    )
+
+    assert len(results.items) == 1
+    assert results.items[0].document_id == doc_2.id
+    assert results.items[0].title == "doc_2.pdf"
+
+
+async def test_filter_and_sort_documents_by_multiple_cf(
+    db_session: AsyncSession,
+    user,
+    make_document_type_with_custom_fields,
+    make_document
+):
+    docs = []
+    for index in range(0, 10):
+        doc = await make_document(
+            title=f"doc_{index}.pdf",
+            user=user,
+            parent=user.home_folder,
+            lang=search_schema.SearchLanguage.ENG
+        )
+        docs.append(doc)
+
+    invoice_type = await make_document_type_with_custom_fields(
+        name="Invoice",
+        custom_fields=[
+            {"name": "Invoice Date", "type_handler": "date"},
+            {"name": "Vendor", "type_handler": "text"},
+            {
+                "name": "Total Amount", "type_handler": "monetary",
+                "config": {"currency": "EUR"}
+            },
+        ]
+    )
+    for doc in docs:
+        await doc_dbapi.update_doc_type(
+            db_session,
+            document_id=doc.id,
+            document_type_id=invoice_type.id
+        )
+
+    Invoice = namedtuple(
+        'Invoice',
+        ['index', 'total_amount', 'vendor', 'invoice_date']
+    )
+
+    values = [
+        Invoice(0, "29.05", "lidl", "2024-12-15"),
+        Invoice(1, "9.0", "lidl", "2024-12-16"),
+        Invoice(2, "17.25", "lidl", "2024-06-13"),
+        Invoice(3, "49.05", "lidl", "2024-05-11"),
+        Invoice(4, "21.03", "lidl", "2024-04-10"),
+        Invoice(5, "19.05", "lidl", "2024-12-15"),
+        Invoice(6, "9.0", "rewe", "2024-11-16"),
+        Invoice(7, "17.25", "rewe", "2024-11-13"),
+        Invoice(8, "49.05", "aldi", "2024-11-11"),
+        Invoice(9, "21.03", "rewe", "2024-08-18"),
+    ]
+
+    for value in values:
+        await cf_dbapi.update_document_custom_field_values(
+            db_session,
+            document_id=docs[value.index].id,
+            custom_fields={
+                "Total Amount": value.total_amount,
+                "Vendor": value.vendor,
+                "Invoice Date": value.invoice_date
+            }
+        )
+
+    params = search_schema.SearchQueryParams(
+        filters=search_schema.SearchFilters(
+            custom_fields=[
+                search_schema.CustomFieldFilter(
+                    field_name="Total Amount",
+                    operator=search_schema.Operator.GT,
+                    value=20
+                ),
+                search_schema.CustomFieldFilter(
+                    field_name="Vendor",
+                    operator=search_schema.Operator.EQ,
+                    value="lidl"
+                )
+            ]
+        ),
+        sort_by="Total Amount",
+        sort_direction=search_schema.SortDirection.DESC,
+        lang=search_schema.SearchLanguage.ENG,
+        document_type_id=invoice_type.id,
+    )
+
+    results = await search_dbapi.search_documents_by_type(
+        db_session=db_session,
+        user_id=user.id,
+        params=params
+    )
+
+    assert len(results.items) == 3
+    actual_document_ids = [doc.document_id for doc in results.items]
+    # 1. doc with amount 49.05
+    # 2. doc with amount 29.05
+    # 3. doc with amount 21.03
+    assert actual_document_ids == [docs[3].id, docs[0].id, docs[4].id]

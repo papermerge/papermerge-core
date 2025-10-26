d612ac6d62d3c55dff3911641a5acf3ed9aa64b2
Eugen Ciur <eugen@papermerge.com>
2025-10-26 07:51:48 +0100

    move plugin part into top level conftest.py

---
+++ papermerge/conftest.py
@@ -0,0 +1,6 @@
+pytest_plugins = [
+    "papermerge.core.features.tags.tests.conftest",
+    # Add other feature conftest modules here as needed
+    # "papermerge.core.features.documents.tests.conftest",
+    # "papermerge.core.features.users.tests.conftest",
+]

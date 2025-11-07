b20b269058381a96543af0b48416a7081d8671f5
Eugen Ciur <ciur.eugen@gmail.com>
2025-11-07 06:56:43 +0100

    minor refinements

--- frontend/apps/ui/src/components/ConditionalTooltip.tsx
+++ frontend/apps/ui/src/components/ConditionalTooltip.tsx
@@ -1,26 +1,17 @@
 import {Tooltip, TooltipProps} from "@mantine/core"
 import {ReactElement} from "react"
 
-interface ConditionalTooltipProps {
-  /**
-   * Whether to show the tooltip wrapper
-   */
-  showTooltipIf: boolean
-  /**
-   * The child element to potentially wrap with a tooltip
-   */
-  children: ReactElement
-  /**
-   * Mantine Tooltip props to apply when condition is true
-   */
-  tooltipProps: Omit<TooltipProps, "children">
+interface Args {
+  showTooltipIf: boolean // Whether to show the tooltip wrapper
+  children: ReactElement //  The child element to potentially wrap with a tooltip
+  tooltipProps: Omit<TooltipProps, "children"> // Mantine Tooltip props to apply when condition is true
 }
 
 export const ConditionalTooltip = ({
   showTooltipIf,
   children,
   tooltipProps
-}: ConditionalTooltipProps) => {
+}: Args) => {
   if (showTooltipIf) {
     return <Tooltip {...tooltipProps}>{children}</Tooltip>
   }

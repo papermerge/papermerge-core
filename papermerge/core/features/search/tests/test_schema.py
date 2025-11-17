967c387224cffd2ea671dbc9c10a96000d56f740
Eugen Ciur <eugen@papermerge.com>
2025-11-17 07:28:53 +0100

    fix tag filtering (#1093)

--- papermerge/core/features/search/tests/test_schema.py
+++ papermerge/core/features/search/tests/test_schema.py
@@ -21,3 +21,50 @@ def test_basic_schema():
     )
 
     assert search_schema.SearchQueryParams(**input) == expected_output
+
+
+
+def test_tag_filters_with_empty_values_1():
+    """tag filters with empty values are removed from the list"""
+    input = {
+        "filters": {
+            "tags": [
+                {"values": ["blue", "green"]},
+                {"values": []},
+                {"values": [], "operator": "any"}
+            ]
+        },
+        "lang": "eng"
+    }
+    expected_output = search_schema.SearchQueryParams(
+        filters=search_schema.SearchFilters(
+            tags=[
+                search_schema.TagFilter(
+                    values=["blue", "green"]
+                )
+            ]
+        ),
+        lang=search_schema.SearchLanguage.ENG
+    )
+
+    assert search_schema.SearchQueryParams(**input) == expected_output
+
+
+def test_tag_filters_with_empty_values_2():
+    """tag filters with empty values are removed from the list"""
+    input = {
+        "filters": {
+            "tags": [
+                {"values": []},
+                {"values": [], "operator": "any"},
+                {"values": [], "operator": "all"}
+            ]
+        },
+        "lang": "eng"
+    }
+    expected_output = search_schema.SearchQueryParams(
+        filters=search_schema.SearchFilters(tags=[]),
+        lang=search_schema.SearchLanguage.ENG
+    )
+
+    assert search_schema.SearchQueryParams(**input) == expected_output

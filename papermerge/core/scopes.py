746ecbe882b90e215a1bea48508ce2d652124a96
Eugen Ciur <eugen@papermerge.com>
2025-12-07 06:50:24 +0100

    refactoring: convenient usage of scopes module (#1162)

--- papermerge/core/scopes.py
+++ papermerge/core/scopes.py
@@ -1,3 +1,15 @@
 from papermerge.core.features.auth.scopes import Scopes
+from papermerge.core.features.auth.dependencies import require_scopes
+from papermerge.core.schema import User
 
-__all__ = [Scopes,]
+ViewCustomFields: type[User] = require_scopes(Scopes.CUSTOM_FIELD_VIEW)
+CreateCustomFields: type[User] = require_scopes(Scopes.CUSTOM_FIELD_CREATE)
+DeleteCustomFields: type[User] = require_scopes(Scopes.CUSTOM_FIELD_DELETE)
+UpdateCustomFields: type[User] = require_scopes(Scopes.CUSTOM_FIELD_UPDATE)
+
+__all__ = [
+    "ViewCustomFields",
+    "CreateCustomFields",
+    "DeleteCustomFields",
+    "UpdateCustomFields"
+]

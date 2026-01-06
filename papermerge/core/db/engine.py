e5965a56fb1b229f407676e689f5a2dfb25589e3
Eugen Ciur <ciur.eugen@gmail.com>
2026-01-06 08:11:39 +0100

    ssl or not ssl

--- papermerge/core/db/engine.py
+++ papermerge/core/db/engine.py
@@ -7,8 +7,15 @@ from papermerge.core.config import settings
 
 logger = logging.getLogger(__name__)
 
+
+connect_args = {}
+if settings.db_ssl:
+    connect_args["ssl"] = "require"
+
 engine = create_async_engine(
-    settings.async_db_url, poolclass=NullPool
+    settings.async_db_url,
+    poolclass=NullPool,
+    connect_args=connect_args
 )
 
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
